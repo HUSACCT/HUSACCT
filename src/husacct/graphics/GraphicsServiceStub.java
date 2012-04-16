5fdd1dc1f25459a0d00b4a5e19d597b9c3364a2d
Johan van der Slikke <johan@johanvds.(none)>
2012-04-16 15:56:08 +0200

    demo gefixt

--- src/husacct/graphics/GraphicsServiceStub.java
+++ src/husacct/graphics/GraphicsServiceStub.java
@@ -1,5 +1,48 @@
 package husacct.graphics;
 
-public class GraphicsServiceStub extends GraphicsServiceImpl implements IGraphicsService
+import husacct.graphics.task.DemoGUIController;
+
+import javax.swing.JInternalFrame;
+
+public class GraphicsServiceStub implements IGraphicsService
 {
+	private DemoGUIController controller;
+	
+	public GraphicsServiceStub()
+	{
+		this.controller = new DemoGUIController();
+	}
+
+	@Override
+	public JInternalFrame getAnalysedArchitectureGUI()
+	{
+		return this.controller.getGUI();
+	}
+
+	@Override
+	public JInternalFrame getDefinedArchitectureGUI()
+	{
+		return this.controller.getGUI();
+	}
+
+	@Override
+	public void drawAnalysedArchitecture()
+	{		
+	}
+
+	@Override
+	public void drawAnalysedArchitectureWithViolations()
+	{
+	}
+
+	@Override
+	public void drawDefinedArchitecture()
+	{		
+	}
+
+	@Override
+	public void drawDefinedArchitectureWithViolations()
+	{		
+	}
+	
 }
