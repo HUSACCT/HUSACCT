21fd1e8a7e7b66147b3f6e7b7f43ef7a27d62ff5
Rene van Aerle <info@rvgate.nl>
2012-04-16 21:49:55 +0200

    Added log4j with basic configuration

--- src/husacct/Main.java
+++ src/husacct/Main.java
@@ -1,10 +1,18 @@
 package husacct;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 import husacct.control.task.MainController;
 
 public class Main {
 	
 	public static void main(String[] args) {
+		
+		BasicConfigurator.configure();
+		
+		Logger logger = Logger.getLogger(Main.class);
+		logger.debug("Starting HUSACCT");
 		new MainController();
 	}
 
