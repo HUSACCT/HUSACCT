00701ba9bf6504e5eba0965899c8b5f74202c06a
Tom de Bruijn <tom@newanz.com>
2012-04-18 15:34:38 +0200

    Very rough start on zooming

--- src/husacct/graphics/task/AnalysedController.java
+++ src/husacct/graphics/task/AnalysedController.java
@@ -4,6 +4,7 @@ import husacct.analyse.AnalyseServiceStub;
 import husacct.analyse.IAnalyseService;
 import husacct.common.dto.AbstractDTO;
 import husacct.common.dto.AnalysedModuleDTO;
+import husacct.graphics.presentation.decorators.DTODecorator;
 import husacct.graphics.presentation.figures.BaseFigure;
 
 public class AnalysedController extends BaseController {
@@ -33,5 +34,12 @@ public class AnalysedController extends BaseController {
 
 	@Override
 	public void moduleZoom(BaseFigure zoomedModuleFigure) {
+		AbstractDTO dto = ((DTODecorator) zoomedModuleFigure).getDTO();
+		switch(dto.getClass().getSimpleName()){
+			case "AnalysedModuleDTO":
+				AnalysedModuleDTO newdto = ((AnalysedModuleDTO)dto);
+				AnalysedModuleDTO[] children = analyseService.getChildModulesInModule(newdto.uniqueName);
+				drawModules(children);
+		}
 	}
 }
