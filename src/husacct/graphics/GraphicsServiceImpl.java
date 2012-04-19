953336625d36dc9db545b02ccc226d284f8dacb7
Tom de Bruijn <tom@newanz.com>
2012-04-19 14:39:19 +0200

    Implementing the save chain in our graphics service.

--- src/husacct/graphics/GraphicsServiceImpl.java
+++ src/husacct/graphics/GraphicsServiceImpl.java
@@ -1,12 +1,15 @@
 package husacct.graphics;
 
+import husacct.common.savechain.ISaveable;
 import husacct.graphics.task.AnalysedController;
 import husacct.graphics.task.DefinedController;
 import husacct.graphics.task.DrawingDetail;
 
 import javax.swing.JInternalFrame;
 
-public class GraphicsServiceImpl implements IGraphicsService {
+import org.jdom2.Element;
+
+public class GraphicsServiceImpl implements IGraphicsService, ISaveable {
 
 	private AnalysedController analysedController;
 	private DefinedController definedController;
@@ -45,5 +48,17 @@ public class GraphicsServiceImpl implements IGraphicsService {
 	@Override
 	public void drawDefinedArchitectureWithViolations() {
 		definedController.drawArchitecture(DrawingDetail.WITH_VIOLATIONS);
+	}
+
+	@Override
+	public Element getWorkspaceData() {
+		Element data = new Element("ArchitecureGraphicsService");
+		data.addContent("testdata");
+		return data;
+	}
+
+	@Override
+	public void loadWorkspaceData(Element workspaceData) {
+		//TODO: Set workspace data.
 	}
 }
