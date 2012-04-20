4a30da15373f03dbf6045087af5cfea65560b065
Maarten <mrtentje@gmail.com>
2012-04-20 17:07:44 +0200

    Implemented configuration of severties per programminglanguage

--- src/husacct/validate/domain/configuration/SeverityPerTypeRepository.java
+++ src/husacct/validate/domain/configuration/SeverityPerTypeRepository.java
@@ -1,10 +1,9 @@
 package husacct.validate.domain.configuration;
 
+import husacct.validate.domain.validation.Severity;
+
 import java.util.HashMap;
 
 public class SeverityPerTypeRepository {
-	
-	//FIXME: Which one to use?
-	private HashMap<String, Integer> severityPerType;
-	//private HashMap<String, Severity> severityPerType
+	private HashMap<String, HashMap<String, Severity>> severitiesPerTypePerProgrammingLanguage;
 }
