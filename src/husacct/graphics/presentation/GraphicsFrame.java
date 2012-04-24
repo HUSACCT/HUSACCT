fa1398639c9791a2301feacaa3b1a7f9f03f1499
Guido van Tricht <guido.vantricht@student.hu.nl>
2012-04-24 14:08:14 +0200

    Menu bar created

--- src/husacct/graphics/presentation/GraphicsFrame.java
+++ src/husacct/graphics/presentation/GraphicsFrame.java
@@ -1,12 +1,15 @@
 package husacct.graphics.presentation;
 
 import javax.swing.JInternalFrame;
+import javax.swing.JMenuBar;
 import javax.swing.JScrollPane;
 
 public class GraphicsFrame extends JInternalFrame {
 	private static final long serialVersionUID = -4683140198375851034L;
 
 	private DrawingView drawingView;
+	
+	private JMenuBar menuBar;
 
 	private javax.swing.JScrollPane scrollPane;
 
@@ -27,5 +30,18 @@ public class GraphicsFrame extends JInternalFrame {
 
 		this.setLayout(new java.awt.BorderLayout());
 		this.add(scrollPane, java.awt.BorderLayout.CENTER);
+		
+		createMenuBar();
+		this.add(menuBar, java.awt.BorderLayout.NORTH);
+	}
+	
+	private void createMenuBar(){
+		this.menuBar = new JMenuBar();
+		
+		this.menuBar = new javax.swing.JMenuBar();
+		javax.swing.JMenu nmenu = new javax.swing.JMenu("North menu");
+		javax.swing.JMenuItem item = new javax.swing.JMenuItem("Level up");
+		nmenu.add(item);
+		menuBar.add(nmenu);
 	}
 }
