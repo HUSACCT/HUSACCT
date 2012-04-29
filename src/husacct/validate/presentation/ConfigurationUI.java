6a0095761dc2ccef5e7619f84c4b0ea08fa06418
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2012-04-29 20:42:21 +0200

    Removed LinkedHashMap import

--- src/husacct/validate/presentation/ConfigurationUI.java
+++ src/husacct/validate/presentation/ConfigurationUI.java
@@ -8,7 +8,6 @@ import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 import javax.swing.*;
 import javax.swing.table.TableCellEditor;
