f65c631527fcf62993d0bc34defba65cd7cb0b1e
Patrick van der Willik <patrick@toolmaker.nl>
2012-05-04 11:31:47 +0200

    Updated the GraphicsServiceImpl.java to fix calls to createControllers() that were on the incorrect location

--- src/husacct/graphics/GraphicsServiceImpl.java
+++ src/husacct/graphics/GraphicsServiceImpl.java
@@ -15,6 +15,7 @@ public class GraphicsServiceImpl implements IGraphicsService, ISaveable {
 	private DefinedController definedController;
 
 	public GraphicsServiceImpl() {
+		createControllers();
 	}
 
 	private void createControllers() {
@@ -28,43 +29,36 @@ public class GraphicsServiceImpl implements IGraphicsService, ISaveable {
 
 	@Override
 	public JInternalFrame getAnalysedArchitectureGUI() {
-		createControllers();
 		return analysedController.getGUI();
 	}
 
 	@Override
 	public JInternalFrame getDefinedArchitectureGUI() {
-		createControllers();
 		return definedController.getGUI();
 	}
 
 	@Override
 	public void drawAnalysedArchitecture() {
-		createControllers();
 		analysedController.drawArchitecture(DrawingDetail.WITHOUT_VIOLATIONS);
 	}
 
 	@Override
 	public void drawAnalysedArchitectureWithViolations() {
-		createControllers();
 		analysedController.drawArchitecture(DrawingDetail.WITH_VIOLATIONS);
 	}
 
 	@Override
 	public void drawDefinedArchitecture() {
-		createControllers();
 		definedController.drawArchitecture(DrawingDetail.WITHOUT_VIOLATIONS);
 	}
 
 	@Override
 	public void drawDefinedArchitectureWithViolations() {
-		createControllers();
 		definedController.drawArchitecture(DrawingDetail.WITH_VIOLATIONS);
 	}
 
 	@Override
 	public Element getWorkspaceData() {
-		createControllers();
 		Element data = new Element("ArchitecureGraphicsService");
 		// TODO: Save violations on or off
 		data.addContent("testdata");
@@ -73,7 +67,6 @@ public class GraphicsServiceImpl implements IGraphicsService, ISaveable {
 
 	@Override
 	public void loadWorkspaceData(Element workspaceData) {
-		createControllers();
 		// TODO: Set workspace data.
 		// - show violations = on/off?
 	}
