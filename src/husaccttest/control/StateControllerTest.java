27f0449cb83a972ca5d56a21cb259687c533687e
Rene van Aerle <info@rvgate.nl>
2012-05-14 15:20:23 +0200

    Rewrote testcases for statecontroller

--- src/husaccttest/control/StateControllerTest.java
+++ src/husaccttest/control/StateControllerTest.java
@@ -1,7 +1,14 @@
 package husaccttest.control;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import husacct.control.task.IStateChangeListener;
 import husacct.control.task.MainController;
 import husacct.control.task.StateController;
+import husacct.control.task.States;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -16,15 +23,35 @@ public class StateControllerTest {
 		stateController = mainController.getStateController();
 	}
 	
-	// TODO: rewrite initial state test
 	@Test
 	public void testInitialState(){
-	
+		List<States> states = stateController.getState();
+		assertTrue(states.contains(States.NONE));
+		assertFalse(states.contains(States.OPENED));
+		assertFalse(states.contains(States.DEFINED));
+		assertFalse(states.contains(States.ANALYSED));
+		assertFalse(states.contains(States.MAPPED));
+		assertFalse(states.contains(States.VALIDATED));
 	}
 	
-	// TODO: rewrite statechange test
 	@Test
 	public void testStateChangeListener(){
-	
+		stateController.addStateChangeListener(new IStateChangeListener() {
+			public void changeState(List<States> states) {
+				assertTrue(states.contains(States.NONE));
+				assertFalse(states.contains(States.OPENED));
+				assertTrue(states.contains(States.DEFINED));
+				assertFalse(states.contains(States.ANALYSED));
+				assertTrue(states.contains(States.MAPPED));
+				assertFalse(states.contains(States.VALIDATED));
+			}
+		});
+		
+		List<States> states = new ArrayList<States>();
+		states.add(States.NONE);
+		states.add(States.DEFINED);
+		states.add(States.MAPPED);
+		
+		stateController.setState(states);
 	}
 }
