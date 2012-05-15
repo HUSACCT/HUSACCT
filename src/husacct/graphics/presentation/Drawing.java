8db7abd459566a1564228cabae699a3537ef737a
Tom de Bruijn <tom@newanz.com>
2012-05-15 10:49:09 +0200

    Using QuadTreeDrawing instead of DefaultDrawing. Should be faster.

--- src/husacct/graphics/presentation/Drawing.java
+++ src/husacct/graphics/presentation/Drawing.java
@@ -13,11 +13,11 @@ import java.util.ArrayList;
 import javax.swing.JFileChooser;
 
 import org.apache.log4j.Logger;
-import org.jhotdraw.draw.DefaultDrawing;
 import org.jhotdraw.draw.Figure;
+import org.jhotdraw.draw.QuadTreeDrawing;
 import org.jhotdraw.draw.io.ImageOutputFormat;
 
-public class Drawing extends DefaultDrawing {
+public class Drawing extends QuadTreeDrawing {
 	private static final long serialVersionUID = 3212318618672284266L;
 	private Logger logger = Logger.getLogger(Drawing.class);
 
