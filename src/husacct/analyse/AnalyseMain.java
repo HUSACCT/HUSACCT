a99f6dd70263f630e1a6b1016a209e0e6421de3e
Thomasch <thomasch@thomasch.nl>
2012-05-16 22:21:48 +0200

    fixed the merge with the upstream

--- src/husacct/analyse/AnalyseMain.java
+++ src/husacct/analyse/AnalyseMain.java
@@ -14,9 +14,9 @@ public class AnalyseMain {
 		ServiceProvider provider = ServiceProvider.getInstance();
 		IDefineService defService = provider.getDefineService();
 		
-		String path = "C:\\Tools\\Workspaces\\School\\TO Blok 3 Jaar 3\\Army\\src";
+		String path = "/home/thomas/Downloads/Mittchel-benchmark-73d2abb/CSharpBenchmark";
 		String[] paths = new String[]{path};
-		defService.createApplication("Boobies Sanders Application", paths, "Java", "1.0");
+		defService.createApplication("Boobies Sanders Application", paths, "C#", "1.0");
 		
 		IAnalyseService analyser = provider.getAnalyseService();
 		analyser.analyseApplication();
@@ -27,6 +27,7 @@ public class AnalyseMain {
 //	     	UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());    
 //	     } catch (Exception ex) { }
 //		
-//		new AnalyseDebuggingFrame();
+		new AnalyseDebuggingFrame();
+		
 	}
 }	
