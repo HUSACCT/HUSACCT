3976e1c89878bc76d9aae34e675a704d85eb9260
Bob Duncan Sanders <bobsanders13@hotmail.com>
2012-05-18 18:42:08 +0200

    -Adding a new module without entering information will now show the correct error message

--- src/husacct/define/task/ValueInputController.java
+++ src/husacct/define/task/ValueInputController.java
@@ -1,10 +1,5 @@
 package husacct.define.task;
 
-/**
- * 
- * @author Henk ter Harmsel
- *
- */
 public class ValueInputController {
 	
 	private String errorMessage = "";
@@ -14,14 +9,7 @@ public class ValueInputController {
 	}
 	
 	public boolean checkModuleNameInput(String nameInput) {
-		if(isNotEmpty(nameInput, "hierarchical level")) {
-			return true;
-		}
-		return false;
-	}
-	
-	public boolean checkHierarchicalLevelInput(String levelInput) {
-		if(isNotEmpty(levelInput, "hierarchical level") && isInteger(levelInput, "hierarchical level")){
+		if(isNotEmpty(nameInput, "module name")) {
 			return true;
 		}
 		return false;
@@ -33,17 +21,6 @@ public class ValueInputController {
 			return false;
 		}
 		return true;
-	}
-	
-	private boolean isInteger(String input, String name) {  
-	   try {  
-	      Integer.parseInt(input);  
-	      return true;  
-	   }  
-	   catch(Exception e) {
-		   this.errorMessage = "Please fill in a " + name;
-	      return false;  
-	   }  
 	}
 
 	public String getErrorMessage() {
