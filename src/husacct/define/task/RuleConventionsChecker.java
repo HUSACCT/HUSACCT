b6b734a0db350f928a4b6ae911be41090ec06b31
Henk ter Harmsel <henkterharmsel92@gmail.com>
2012-05-22 20:04:46 +0200

    Small bug fix when ruletypecheck is not implemented yet

--- src/husacct/define/task/RuleConventionsChecker.java
+++ src/husacct/define/task/RuleConventionsChecker.java
@@ -25,7 +25,7 @@ public class RuleConventionsChecker {
 	
 	public boolean checkRuleConventions() {
 		fillAppliedRules();
-		boolean conventionError = false;
+		boolean conventionError = true;
 		if(ruleTypeKey.equals("VisibilityConvention")) {
 			conventionError = checkVisibilityConvention();
 		} else if(ruleTypeKey.equals("VisibilityConventionException")) {
@@ -34,6 +34,8 @@ public class RuleConventionsChecker {
 			conventionError = checkNamingConvention();
 		} else if(ruleTypeKey.equals("NamingConventionException")) {
 			conventionError = checkNamingConventionException();
+		} else if(ruleTypeKey.equals("IsNotAllowedToUse")) {
+			conventionError = checkIsNotAllowedToUse();
 		}
 		return conventionError;
 	}
@@ -67,10 +69,27 @@ public class RuleConventionsChecker {
 		return namingConventionError;
 	}
 	
+	private boolean checkIsNotAllowedToUse() {
+		boolean isNotAllowedToUseError = checkRuleTypeAlreadyFromToSelected("IsOnlyAllowedToUse");
+		return isNotAllowedToUseError;
+	}
+	
 	private boolean checkRuleTypeAlreadySet() {
 		for(AppliedRule appliedRule : appliedRules) {
 			if(appliedRule.getRuleType().equals(ruleTypeKey)) {
 				setErrorKey("RuleTypeAlreadySet");
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private boolean checkRuleTypeAlreadyFromToSelected(String ruleType) {
+		for(AppliedRule appliedRule : appliedRules) {
+			if(appliedRule.getRuleType().equals(ruleType) &&
+				appliedRule.getModuleFrom().getId() == moduleFrom.getId() &&
+				appliedRule.getModuleTo().getId() == moduleTo.getId()) {
+				setErrorKey(ruleType + "AlreadyFromToSelected");
 				return false;
 			}
 		}
