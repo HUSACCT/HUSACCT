d5f17c248c9c989f487dc1980ea53ecd3eeb30ba
Tim Muller <t_muller@hotmail.com>
2012-05-22 17:15:04 +0200

    added testcases

--- src/husaccttest/analyse/benchmarkLeo/BenchmarkTestSuite.java
+++ src/husaccttest/analyse/benchmarkLeo/BenchmarkTestSuite.java
@@ -5,6 +5,7 @@ import husaccttest.analyse.benchmarkLeo.declarations.DeclarationInnerPackage;
 import husaccttest.analyse.benchmarkLeo.declarations.DeclarationOuterPackage;
 import husaccttest.analyse.benchmarkLeo.domain.Blog;
 import husaccttest.analyse.benchmarkLeo.domain.Facebook;
+import husaccttest.analyse.benchmarkLeo.exception.ExceptionDependency;
 import husaccttest.analyse.benchmarkLeo.exception.ExceptionDirectoryStructure;
 
 import org.junit.runner.RunWith;
@@ -18,7 +19,8 @@ import org.junit.runners.Suite;
 	DeclarationDirectoryStructure.class,
 	DeclarationInnerPackage.class,
 	DeclarationOuterPackage.class,
-	ExceptionDirectoryStructure.class
+	ExceptionDirectoryStructure.class,
+	ExceptionDependency.class
 })
 public class BenchmarkTestSuite {
 
