ca80a4dc22de7396bd7bfaabce2cf672cb69ff41
Rene van Aerle <info@rvgate.nl>
2012-05-22 23:26:35 +0200

    Added system info on startup

--- src/husacct/Main.java
+++ src/husacct/Main.java
@@ -2,21 +2,39 @@ package husacct;
 
 import husacct.control.IControlService;
 
+import java.lang.management.RuntimeMXBean;
 import java.net.URL;
 
+import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
+
+import sun.management.ManagementFactory;
 
 public class Main {
 	
 	public Main(String[] consoleArguments){
 		setLog4jConfiguration();
+		printSystemInfo();
 		IControlService controlService = ServiceProvider.getInstance().getControlService();
 		controlService.startApplication(consoleArguments);
 	}
 	
-	public void setLog4jConfiguration(){
+	private void setLog4jConfiguration(){
 		URL propertiesFile = getClass().getResource("/husacct/common/resources/husacct.properties");
 		PropertyConfigurator.configure(propertiesFile);
+	}
+	
+	private void printSystemInfo(){
+		Logger logger = Logger.getLogger(Main.class);
+		Runtime runtime = Runtime.getRuntime();
+		RuntimeMXBean mxBean = ManagementFactory.getRuntimeMXBean();
+		
+		logger.debug("Available processors: " + runtime.availableProcessors());
+		logger.debug("Free memory: " + runtime.freeMemory());
+		logger.debug("Max memory: " + runtime.maxMemory());
+		logger.debug("Total memory: " + runtime.totalMemory());
+		logger.debug("Process: " + mxBean.getName());
+		
 	}
 	
 	public static void main(String[] consoleArguments) {
