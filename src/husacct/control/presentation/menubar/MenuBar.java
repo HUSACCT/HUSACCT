3ebe0672411ab1d930f1fb5b0f5da4e12621399f
Erik Verhoef <verhoef_erik@hotmail.com>
2012-05-23 18:16:41 +0200

    fixed issue in menubar

--- src/husacct/control/presentation/menubar/MenuBar.java
+++ src/husacct/control/presentation/menubar/MenuBar.java
@@ -12,26 +12,32 @@ import javax.swing.KeyStroke;
 
 public class MenuBar extends JMenuBar{
 	private static final long serialVersionUID = 1L;
-	
-	FileMenu fileMenu;
-	DefineMenu defineMenu;
-	AnalyseMenu analyseMenu;
-	ValidateMenu validateMenu;
-	LanguageMenu languageMenu;
-	HelpMenu helpMenu;
-	
-	IControlService controlService;
-	
+
+	private FileMenu fileMenu;
+	private DefineMenu defineMenu;
+	private AnalyseMenu analyseMenu;
+	private ValidateMenu validateMenu;
+	private LanguageMenu languageMenu;
+	private HelpMenu helpMenu;
+
+	private IControlService controlService;
+	private MainController mainController;
+
 	public MenuBar(MainController mainController){
-		
+		this.mainController = mainController;
+		addComponents();
+		addListeners();
+	}
+
+	private void addComponents() {
 		controlService = ServiceProvider.getInstance().getControlService();
-		
+
 		fileMenu = new FileMenu(mainController);
 		defineMenu = new DefineMenu(mainController);
 		analyseMenu = new AnalyseMenu(mainController);
 		validateMenu = new ValidateMenu(mainController);
 		languageMenu = new LanguageMenu(mainController);
-		helpMenu = new HelpMenu(mainController);
+		helpMenu = new HelpMenu(mainController);		
 
 		fileMenu.setMnemonic(getMnemonicKeycode("FileMenuMnemonic"));
 		defineMenu.setMnemonic(getMnemonicKeycode("DefineMenuMnemonic"));
@@ -39,17 +45,16 @@ public class MenuBar extends JMenuBar{
 		validateMenu.setMnemonic(getMnemonicKeycode("ValidateMenuMnemonic"));
 		languageMenu.setMnemonic(getMnemonicKeycode("LanguageMenuMnemonic"));
 		helpMenu.setMnemonic(getMnemonicKeycode("HelpMenuMnemonic"));
-		
+
 		add(fileMenu);
 		add(defineMenu);
 		add(analyseMenu);
 		add(validateMenu);
 		add(languageMenu);
 		add(helpMenu);
-		
 	}
-	
-	public void addListeners(){
+
+	private void addListeners(){
 		controlService.addLocaleChangeListener(new ILocaleChangeListener() {
 			public void update(Locale newLocale) {
 				fileMenu.setMnemonic(getMnemonicKeycode("FileMenuMnemonic"));
@@ -61,25 +66,25 @@ public class MenuBar extends JMenuBar{
 			}
 		});
 	}
-	
+
 	private int getMnemonicKeycode(String translatedString) {
 		String mnemonicString = controlService.getTranslatedString(translatedString);
 		int keyCode = KeyStroke.getKeyStroke(mnemonicString).getKeyCode();
 		return keyCode;
 	}
-	
+
 	public FileMenu getFileMenu(){
 		return fileMenu;
 	}
-	
+
 	public DefineMenu getDefineMenu(){
 		return defineMenu;
 	}
-	
+
 	public AnalyseMenu getAnalyseMenu(){
 		return analyseMenu;
 	}
-	
+
 	public ValidateMenu getValidateMenu(){
 		return validateMenu;
 	}
