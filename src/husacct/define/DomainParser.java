d77509ae8acb471217edbca7bf3f7783bda3fe3c
Bob Duncan Sanders <bobsanders13@hotmail.com>
2012-05-23 17:00:38 +0200

    -SoftwareUnit type method changed to interface
    -Define now recursively adds all child paths to the path list

--- src/husacct/define/DomainParser.java
+++ src/husacct/define/DomainParser.java
@@ -2,6 +2,8 @@ package husacct.define;
 
 import java.util.ArrayList;
 
+import husacct.ServiceProvider;
+import husacct.common.dto.AnalysedModuleDTO;
 import husacct.common.dto.ApplicationDTO;
 import husacct.common.dto.ModuleDTO;
 import husacct.common.dto.PhysicalPathDTO;
@@ -10,6 +12,7 @@ import husacct.define.domain.Application;
 import husacct.define.domain.AppliedRule;
 import husacct.define.domain.SoftwareArchitecture;
 import husacct.define.domain.SoftwareUnitDefinition;
+import husacct.define.domain.SoftwareUnitDefinition.Type;
 import husacct.define.domain.module.Module;
 
 public class DomainParser {
@@ -52,10 +55,9 @@ public class DomainParser {
 	
 	public ModuleDTO parseModule(Module module){
 		String logicalPath = getLogicalPath(module);
-		String[] physicalPaths = module.getPhysicalPaths();
-		PhysicalPathDTO[] physicalPathDTOs = parsePhysicalPathDTOs(module.getUnits());
+		ArrayList<SoftwareUnitDefinition> expandedSoftwareUnits = getExpandedSoftwareUnits(module.getUnits());
+		PhysicalPathDTO[] physicalPathDTOs = parsePhysicalPathDTOs(expandedSoftwareUnits);
 		String type = module.getType();
-		
 		
 		ArrayList<ModuleDTO> subModuleDTOsList = new ArrayList<ModuleDTO>();
 		for (Module subModule : module.getSubModules()){
@@ -67,20 +69,57 @@ public class DomainParser {
 		subModuleDTOsList.toArray(subModuleDTOs);
 		ModuleDTO[] subModules = subModuleDTOs; 
 		
-		ModuleDTO modDTO = new ModuleDTO(logicalPath, physicalPaths,physicalPathDTOs, type, subModules);
+		ModuleDTO modDTO = new ModuleDTO(logicalPath, physicalPathDTOs, type, subModules);
 		return modDTO;
 	}
 	
+
 	public ModuleDTO parseRootModule(Module module){
 		String logicalPath = getLogicalPath(module);
-		String[] physicalPaths = module.getPhysicalPaths();
-		PhysicalPathDTO[] physicalPathDTOs = parsePhysicalPathDTOs(module.getUnits());
+		ArrayList<SoftwareUnitDefinition> expandedSoftwareUnits = getExpandedSoftwareUnits(module.getUnits());
+		PhysicalPathDTO[] physicalPathDTOs = parsePhysicalPathDTOs(expandedSoftwareUnits);
 		
 		String type = module.getType();
 		ModuleDTO[] subModules = new ModuleDTO[0]; 
 		
-		ModuleDTO modDTO = new ModuleDTO(logicalPath, physicalPaths,physicalPathDTOs, type, subModules);
+		ModuleDTO modDTO = new ModuleDTO(logicalPath, physicalPathDTOs, type, subModules);
 		return modDTO;
+	}
+	
+	/**
+	 * SoftwareUnits
+	 */
+	private ArrayList<SoftwareUnitDefinition> getExpandedSoftwareUnits(ArrayList<SoftwareUnitDefinition> units) {
+		ArrayList<SoftwareUnitDefinition> softwareUnits = new ArrayList<SoftwareUnitDefinition>();
+		for (SoftwareUnitDefinition su : units){
+			if (su.getType() == Type.PACKAGE){
+				softwareUnits.addAll(getAllChildModules(su));
+			}
+			softwareUnits.add(su);
+		}
+		return softwareUnits;
+	}
+	
+	private ArrayList<SoftwareUnitDefinition> getAllChildModules(SoftwareUnitDefinition su) {
+		ArrayList<SoftwareUnitDefinition> softwareUnits = new ArrayList<SoftwareUnitDefinition>();
+		AnalysedModuleDTO[] analysedSubModuleDTOs = ServiceProvider.getInstance().getAnalyseService().getChildModulesInModule(su.getName());
+		
+		for (AnalysedModuleDTO am : analysedSubModuleDTOs){
+			SoftwareUnitDefinition softwareUnit = parseAnalysedModuleDTO(am);
+			if (softwareUnit.getType() == Type.PACKAGE){
+				ArrayList<SoftwareUnitDefinition> subSoftwareUnits = getAllChildModules(softwareUnit);
+				softwareUnits.addAll(subSoftwareUnits);
+			}
+			softwareUnits.add(softwareUnit);
+		}
+		return softwareUnits;
+	}
+	
+	private SoftwareUnitDefinition parseAnalysedModuleDTO(AnalysedModuleDTO analysedModuleDTO){
+		String name = analysedModuleDTO.uniqueName;
+		Type type = Type.valueOf(analysedModuleDTO.type.toUpperCase());
+		SoftwareUnitDefinition softwareUnit = new SoftwareUnitDefinition(name, type);
+		return softwareUnit;
 	}
 	
 	/**
