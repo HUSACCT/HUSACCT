d5866898b21b63f21c2bb297f01089ed18c7b30c
Maarten <mrtentje@gmail.com>
2012-06-03 23:54:16 +0200

    Performance improvement of InterfaceConventionRule.java

--- src/husacct/validate/domain/validation/ruletype/contentsofamodule/InterfaceConventionRule.java
+++ src/husacct/validate/domain/validation/ruletype/contentsofamodule/InterfaceConventionRule.java
@@ -13,14 +13,21 @@ import husacct.validate.domain.validation.ruletype.RuleTypes;
 
 import java.util.ArrayList;
 import java.util.EnumSet;
+import java.util.HashSet;
 import java.util.List;
 
 public class InterfaceConventionRule extends RuleType {
 
 	private final static EnumSet<RuleTypes> exceptionrules = EnumSet.of(RuleTypes.IS_ALLOWED);
 
+	private HashSet<String> interfaceCache;
+	private HashSet<String> noInterfaceCache;
+
 	public InterfaceConventionRule(String key, String category, List<ViolationType> violationtypes, Severity severity) {
 		super(key, category, violationtypes, exceptionrules, severity);
+		
+		this.interfaceCache = new HashSet<String>();
+		this.noInterfaceCache = new HashSet<String>();
 	}
 
 	@Override
@@ -31,12 +38,13 @@ public class InterfaceConventionRule extends RuleType {
 		List<Mapping> physicalClasspathsFrom = mappings.getMappingFrom();
 		List<Mapping> physicalClasspathsTo = mappings.getMappingTo();
 
+		DependencyDTO[] dependencies = analyseService.getAllDependencies();
+
 		for(Mapping classPathFrom : physicalClasspathsFrom){	
 			int interfaceCounter = 0;
 			for(Mapping classPathTo : physicalClasspathsTo){
-				DependencyDTO[] dependencies = analyseService.getDependencies(classPathFrom.getPhysicalPath(), classPathTo.getPhysicalPath());
 				for(DependencyDTO dependency : dependencies){
-					if(analyseService.getModuleForUniqueName(dependency.to).type.toLowerCase().equals("interface")){
+					if(dependency.from.equals(classPathFrom.getPhysicalPath()) && dependency.to.equals(classPathTo.getPhysicalPath()) && isInterface(dependency.to)){
 						interfaceCounter++;
 					}
 				}
@@ -47,5 +55,25 @@ public class InterfaceConventionRule extends RuleType {
 			}
 		}
 		return violations;
+	}
+
+	private boolean isInterface(String classPath){
+		if(interfaceCache.contains(classPath)){
+			return true;
+		}
+		else if(noInterfaceCache.contains(classPath)){
+			return false;
+		}
+		else{
+			boolean isInterface = analyseService.getModuleForUniqueName(classPath).type.toLowerCase().equals("interface");
+			if(isInterface){
+				interfaceCache.add(classPath);
+				return true;
+			}
+			else{
+				noInterfaceCache.add(classPath);
+				return false;
+			}
+		}
 	}
 }
