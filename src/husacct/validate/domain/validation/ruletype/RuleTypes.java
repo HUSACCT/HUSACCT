eebd9746d182d250e436c33375d1431953d67d95
Maarten <mrtentje@gmail.com>
2012-06-15 10:58:45 +0200

    Changed the default severity of IsNotAllowedToUseRule

--- src/husacct/validate/domain/validation/ruletype/RuleTypes.java
+++ src/husacct/validate/domain/validation/ruletype/RuleTypes.java
@@ -5,14 +5,13 @@ import husacct.validate.domain.validation.DefaultSeverities;
 import java.util.EnumSet;
 
 public enum RuleTypes {
-	IS_NOT_ALLOWED("IsNotAllowedToUse", DefaultSeverities.MEDIUM),
+	IS_NOT_ALLOWED("IsNotAllowedToUse", DefaultSeverities.HIGH),
 	IS_ALLOWED("IsAllowedToUse", DefaultSeverities.LOW),
 	IS_ONLY_ALLOWED("IsOnlyAllowedToUse", DefaultSeverities.LOW),
 	IS_ONLY_MODULE_ALLOWED("IsOnlyModuleAllowedToUse", DefaultSeverities.MEDIUM),
 	MUST_USE("MustUse", DefaultSeverities.MEDIUM),
 	BACK_CALL("BackCall", DefaultSeverities.HIGH),
 	SKIP_CALL("SkipCall", DefaultSeverities.LOW),
-
 	NAMING_CONVENTION("NamingConvention", DefaultSeverities.MEDIUM),
 	NAMING_CONVENTION_EXCEPTION("NamingConventionException", DefaultSeverities.MEDIUM),
 	VISIBILITY_CONVENTION("VisibilityConvention", DefaultSeverities.MEDIUM),
