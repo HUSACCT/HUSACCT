7082f9b19a6476bb54d50ea038fa2dd75a672283
Rene van Aerle <info@rvgate.nl>
2012-07-01 13:13:54 +0200

    Set java benchmark bootstrap now sets correct path independent of operating system. #152

--- src/husacct/bootstrap/SetJavaBenchmark.java
+++ src/husacct/bootstrap/SetJavaBenchmark.java
@@ -1,10 +1,12 @@
 package husacct.bootstrap;
 
+import java.io.File;
+
 public class SetJavaBenchmark extends AbstractBootstrap{
 	
 	@Override
 	public void execute() {
-		getDefineService().createApplication("Java Benchmark", new String[]{new java.io.File("").getAbsolutePath() + "\\testprojects\\java\\benchmark"}, "Java", "1.0");
+		getDefineService().createApplication("Java Benchmark", new String[]{new File("").getAbsolutePath() + File.separator + "testprojects" + File.separator + "java" + File.separator + "benchmark"}, "Java", "1.0");
 	}
 
 }
