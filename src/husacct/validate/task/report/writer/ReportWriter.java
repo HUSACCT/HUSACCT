92f20e6baaeea1c787a1999bcaa4e8051b0d176f
Maarten <mrtentje@gmail.com>
2012-07-07 15:02:53 +0200

    #168 More reliable way to ask the seperator char
    Changed System.getProperty("file.separator") to File.seperator

--- src/husacct/validate/task/report/writer/ReportWriter.java
+++ src/husacct/validate/task/report/writer/ReportWriter.java
@@ -64,6 +64,6 @@ public abstract class ReportWriter {
 	}
 
 	protected String getFileName() {
-		return path + System.getProperty("file.separator") + fileName;
+		return path + File.separator + fileName;
 	}
 }
