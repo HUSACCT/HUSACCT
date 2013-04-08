cf9f310328f4bd60e350ddf2d6abb0d68e668322
Nathan Rhijnsburger <nathan.rhijnsburger@gmail.com>
2013-04-08 14:52:33 +0200

    Toevoegen verwijderde files

---
+++ src/husacct/validate/domain/validation/ruletype/RuleType.java
@@ -0,0 +1,146 @@
+package husacct.validate.domain.validation.ruletype;
+
+import husacct.ServiceProvider;
+import husacct.analyse.IAnalyseService;
+import husacct.common.dto.DependencyDTO;
+import husacct.common.dto.RuleDTO;
+import husacct.define.IDefineService;
+import husacct.validate.domain.check.util.CheckConformanceUtilSeverity;
+import husacct.validate.domain.configuration.ConfigurationServiceImpl;
+import husacct.validate.domain.exception.ViolationTypeNotFoundException;
+import husacct.validate.domain.factory.violationtype.AbstractViolationType;
+import husacct.validate.domain.factory.violationtype.ViolationTypeFactory;
+import husacct.validate.domain.validation.Message;
+import husacct.validate.domain.validation.Severity;
+import husacct.validate.domain.validation.Violation;
+import husacct.validate.domain.validation.ViolationType;
+import husacct.validate.domain.validation.internal_transfer_objects.Mapping;
+import husacct.validate.domain.validation.internal_transfer_objects.Mappings;
+import husacct.validate.domain.validation.logicalmodule.LogicalModule;
+import husacct.validate.domain.validation.logicalmodule.LogicalModules;
+
+import java.util.EnumSet;
+import java.util.List;
+
+public abstract class RuleType {
+	protected final String key;
+	protected final String descriptionKey;
+	protected final String categoryKey;
+	protected final EnumSet<RuleTypes> exceptionRuleKeys;
+	protected final List<ViolationType> violationtypes;
+	protected List<RuleType> exceptionRules;
+	protected final Severity severity;
+
+	protected List<Violation> violations;
+	protected Mappings mappings;
+	protected List<Mapping> physicalClasspathsFrom;
+
+	protected final IAnalyseService analyseService = ServiceProvider.getInstance().getAnalyseService();
+	protected final IDefineService defineService = ServiceProvider.getInstance().getDefineService();
+
+	private AbstractViolationType violationtypefactory;
+
+	public RuleType(String key, String categoryKey, List<ViolationType> violationtypes, EnumSet<RuleTypes> exceptionRuletypes, Severity severity) {
+		this.key = key;
+		this.descriptionKey = key + "Description";
+		this.categoryKey = categoryKey;
+		this.violationtypes = violationtypes;
+		this.exceptionRuleKeys = exceptionRuletypes;
+		this.severity = severity;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public String getDescriptionKey() {
+		return descriptionKey;
+	}
+
+	public String getCategoryKey() {
+		return categoryKey;
+	}
+
+	public EnumSet<RuleTypes> getExceptionRuleKeys() {
+		return exceptionRuleKeys;
+	}
+
+	public List<ViolationType> getViolationTypes() {
+		return violationtypes;
+	}
+
+	public void setExceptionrules(List<RuleType> ruletypes) {
+		this.exceptionRules = ruletypes;
+	}
+
+	public List<RuleType> getExceptionrules() {
+		return exceptionRules;
+	}
+
+	public Severity getSeverity() {
+		return severity;
+	}
+
+	public abstract List<Violation> check(ConfigurationServiceImpl configuration, RuleDTO rootRule, RuleDTO currentRule);
+
+	protected Violation createViolation(RuleDTO rootRule, Mapping classPathFrom, Mapping classPathTo, DependencyDTO dependency, ConfigurationServiceImpl configuration) {
+		initializeViolationTypeFactory(configuration);
+		Message message = new Message(rootRule);
+
+		LogicalModule logicalModuleFrom = new LogicalModule(classPathFrom);
+		LogicalModule logicalModuleTo = new LogicalModule(classPathTo);
+		LogicalModules logicalModules = new LogicalModules(logicalModuleFrom, logicalModuleTo);
+
+		final Severity violationTypeSeverity = getViolationTypeSeverity(dependency.type);
+		Severity severity = CheckConformanceUtilSeverity.getSeverity(configuration, this.severity, violationTypeSeverity);
+
+		return new Violation(dependency.lineNumber, severity.clone(), this.key, dependency.type, dependency.from, dependency.to, dependency.isIndirect, message, logicalModules);
+	}
+
+	protected Violation createViolation(RuleDTO rootRule, LogicalModules logicalModules, ConfigurationServiceImpl configuration) {
+		initializeViolationTypeFactory(configuration);
+		Message message = new Message(rootRule);
+		Severity severity = CheckConformanceUtilSeverity.getSeverity(configuration, this.severity, null);
+		return new Violation(severity.clone(), this.key, false, message, logicalModules);
+
+	}
+
+	protected Violation createViolation(RuleDTO rootRule, Mapping classPathFrom, ConfigurationServiceImpl configuration) {
+		initializeViolationTypeFactory(configuration);
+		Message message = new Message(rootRule);
+
+		LogicalModule logicalModuleFrom = new LogicalModule(classPathFrom);
+		LogicalModules logicalModules = new LogicalModules(logicalModuleFrom);
+
+		Severity severity = CheckConformanceUtilSeverity.getSeverity(configuration, this.severity, null);
+		return new Violation(severity.clone(), this.key, classPathFrom.getPhysicalPath(), false, message, logicalModules);
+	}
+
+	protected Violation createViolation(RuleDTO rootRule, Mapping classPathFrom, String violationTypeKey, ConfigurationServiceImpl configuration) {
+		initializeViolationTypeFactory(configuration);
+		Message message = new Message(rootRule);
+		LogicalModule logicalModuleFrom = new LogicalModule(classPathFrom);
+		LogicalModules logicalModules = new LogicalModules(logicalModuleFrom);
+
+		final Severity violationTypeSeverity = getViolationTypeSeverity(violationTypeKey);
+
+		Severity severity = CheckConformanceUtilSeverity.getSeverity(configuration, this.severity, violationTypeSeverity);
+		return new Violation(severity.clone(), this.key, violationTypeKey, classPathFrom.getPhysicalPath(), false, message, logicalModules);
+	}
+
+	private void initializeViolationTypeFactory(ConfigurationServiceImpl configuration) {
+		if (violationtypefactory == null) {
+			this.violationtypefactory = new ViolationTypeFactory().getViolationTypeFactory(configuration);
+		}
+	}
+
+	private Severity getViolationTypeSeverity(String violationTypeKey) {
+		try {
+			return violationtypefactory.createViolationType(this.key, violationTypeKey).getSeverity();
+		}
+		catch (ViolationTypeNotFoundException e) {
+
+		}
+		return null;
+	}
+}
---
+++ src/husacct/validate/domain/validation/violationtype/CSharpViolationTypes.java
@@ -0,0 +1,50 @@
+package husacct.validate.domain.validation.violationtype;
+
+import husacct.validate.domain.validation.DefaultSeverities;
+
+public enum CSharpViolationTypes implements IViolationType {
+	ACCESS_PROPERTY_OR_FIELD("AccessPropertyOrField", ViolationCategories.DEPENDENCY_RECOGNITION, DefaultSeverities.MEDIUM), 
+	ANNOTATION("Annotation", ViolationCategories.DEPENDENCY_RECOGNITION, DefaultSeverities.LOW), 
+	DECLARATION("Declaration", ViolationCategories.DEPENDENCY_RECOGNITION, DefaultSeverities.MEDIUM), 
+	EXCEPTION("Exception", ViolationCategories.DEPENDENCY_RECOGNITION, DefaultSeverities.MEDIUM), 
+	EXTENDS_CONCRETE("ExtendsConcrete", ViolationCategories.DEPENDENCY_RECOGNITION, DefaultSeverities.MEDIUM), 
+	EXTENDS_ABSTRACT("ExtendsAbstract", ViolationCategories.DEPENDENCY_RECOGNITION, DefaultSeverities.MEDIUM), 
+	EXTENDS_LIBRARY("ExtendsLibrary", ViolationCategories.DEPENDENCY_RECOGNITION, DefaultSeverities.LOW), 
+	EXTENDS_INTERFACE("ExtendsInterface", ViolationCategories.DEPENDENCY_RECOGNITION, DefaultSeverities.LOW), 
+	IMPLEMENTS("Implements", ViolationCategories.DEPENDENCY_RECOGNITION, DefaultSeverities.MEDIUM), 
+	IMPORT("Import", ViolationCategories.DEPENDENCY_RECOGNITION, DefaultSeverities.LOW), 
+	INVOC_CONSTRUCTOR("InvocConstructor", ViolationCategories.DEPENDENCY_RECOGNITION, DefaultSeverities.MEDIUM), 
+	INVOC_METHOD("InvocMethod", ViolationCategories.DEPENDENCY_RECOGNITION, DefaultSeverities.MEDIUM),
+
+	PUBLIC("public", ViolationCategories.ACCESS_MODIFIERS, DefaultSeverities.MEDIUM), 
+	PROTECTED("protected", ViolationCategories.ACCESS_MODIFIERS, DefaultSeverities.MEDIUM), 
+	DEFAULT("default", ViolationCategories.ACCESS_MODIFIERS, DefaultSeverities.LOW), 
+	PRIVATE("private", ViolationCategories.ACCESS_MODIFIERS, DefaultSeverities.LOW),
+	PACKAGE("package", ViolationCategories.PACKAGING, DefaultSeverities.LOW), 
+	CLASS("class", ViolationCategories.PACKAGING, DefaultSeverities.LOW);
+
+	private final String key;
+	private final DefaultSeverities defaultSeverity;
+	private final ViolationCategories violationCategory;
+
+	CSharpViolationTypes(String value, ViolationCategories violationCategory, DefaultSeverities defaultSeverity) {
+		this.key = value;
+		this.defaultSeverity = defaultSeverity;
+		this.violationCategory = violationCategory;
+	}
+
+	@Override
+	public String toString() {
+		return key;
+	}
+
+	@Override
+	public String getCategory() {
+		return violationCategory.toString();
+	}
+
+	@Override
+	public DefaultSeverities getDefaultSeverity() {
+		return defaultSeverity;
+	}
+}
