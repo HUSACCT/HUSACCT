6ae0821cd781f2bf7602acf5052472415e324e3f
Ali Korkmaz <aleeyk@hotmail.com>
2013-04-08 16:11:43 +0200

    Made some changes for make possibility to find ext. JAR files

--- src/husacct/analyse/task/analyser/java/JavaLibraryGenerator.java
+++ src/husacct/analyse/task/analyser/java/JavaLibraryGenerator.java
@@ -1,5 +1,16 @@
 package husacct.analyse.task.analyser.java;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+
 import org.antlr.runtime.tree.CommonTree;
 import org.antlr.runtime.tree.Tree;
 
@@ -11,7 +22,8 @@ class JavaLibraryGenerator extends JavaGenerator{
 		String name = getNameOfLibrary(uniqueName);
 		createLibrary(name, uniqueName, belongsToLibrary);
 		if(hasParentLibraries(uniqueName)) {
-			createAllParentLibraries(belongsToLibrary);
+			//getResourceListing(String path);
+			//createAllParentLibraries(belongsToLibrary);
 		}
 		return uniqueName;
 	}
@@ -31,12 +43,12 @@ class JavaLibraryGenerator extends JavaGenerator{
 		uniqueName = uniqueName.replace("(import", "");
 		uniqueName = uniqueName.replace("(","");
 		uniqueName = uniqueName.replace(")", "");
-		uniqueName = uniqueName.replace(". ","").substring(1); //trim first space character
+		uniqueName = uniqueName.replace(". ","").substring(1);
 		uniqueName = uniqueName.replace(" ", ".");
 		return uniqueName;
 	}
 	
-	private void createAllParentLibraries(String uniqueChildLibraryName){
+	/*private void createAllParentLibraries(String uniqueChildLibraryName){
 		String belongsToLibrary = "";
 		String name = "";
 		if(hasParentLibraries(uniqueChildLibraryName)){
@@ -50,7 +62,49 @@ class JavaLibraryGenerator extends JavaGenerator{
 		if(hasParentLibraries(uniqueChildLibraryName)){
 			createAllParentLibraries(belongsToLibrary);
 		}
+	}*/
+	
+	private String[] getJarPath(String path) throws URISyntaxException, IOException {
+		String strippedPath = path.substring(1);
+		URL dirURL = getClass().getClassLoader().getResource(strippedPath);
+		if (dirURL != null && dirURL.getProtocol().equals("file")) {
+			/* A file path: easy enough */
+			return new File(dirURL.toURI()).list();
+		} 
+
+		if (dirURL == null) {
+			/* 
+			 * In case of a jar file, we can't actually find a directory.
+			 * Have to assume the same jar as clazz.
+			 */
+			String me = getClass().getName().replace(".", "/")+".class";
+			dirURL = getClass().getClassLoader().getResource(me);
+		}
+
+		if (dirURL.getProtocol().equals("jar")) {
+			/* A JAR path */
+			String jarPath = dirURL.getPath().substring(5, dirURL.getPath().indexOf("!")); //strip out only the JAR file
+			JarFile jar = new JarFile(URLDecoder.decode(jarPath, "UTF-8"));
+			Enumeration<JarEntry> entries = jar.entries(); //gives ALL entries in jar
+			Set<String> result = new HashSet<String>(); //avoid duplicates in case it is a subdirectory
+			while(entries.hasMoreElements()) {
+				String name = entries.nextElement().getName();
+				if (name.startsWith(strippedPath)) { //filter according to the path
+					String entry = name.substring(strippedPath.length());
+					int checkSubdir = entry.indexOf("/");
+					if (checkSubdir >= 0) {
+						// if it is a subdirectory, we just return the directory name
+						entry = entry.substring(0, checkSubdir);
+					}
+					result.add(entry);
+				}
+			}
+			return result.toArray(new String[result.size()]);
+		} 
+
+		throw new UnsupportedOperationException("Cannot list files for URL "+dirURL);
 	}
+
 	
 	private String getNameOfLibrary(String completeLibraryName){
 		String[] allLibraries = splitLibraries(completeLibraryName);
