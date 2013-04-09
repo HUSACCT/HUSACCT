77147a830b5d6cba09a73ac57ef05161700cabe2
Tim Bazuin <krageon@gmail.com>
2013-04-09 12:21:32 +0200

    added todo to spot where rest of everything needs to be added to zoom context for context zoom

--- src/husacct/graphics/task/AnalysedController.java
+++ src/husacct/graphics/task/AnalysedController.java
@@ -18,7 +18,7 @@ import java.util.Set;
 import org.apache.log4j.Logger;
 
 public class AnalysedController extends DrawingController {
-	private Logger logger = Logger.getLogger(AnalysedController.class);
+	private final Logger logger = Logger.getLogger(AnalysedController.class);
 	protected IAnalyseService analyseService;
 	protected IValidateService validateService;
 
@@ -26,145 +26,45 @@ public class AnalysedController extends DrawingController {
 
 	public AnalysedController() {
 		super();
-		initializeServices();
-	}
-	
-	private void initializeServices() {
-		analyseService = ServiceProvider.getInstance().getAnalyseService();
-		analyseService.addServiceListener(new IServiceListener() {
-			@Override
-			public void update() {
-				refreshDrawing();
-			}
-		});
-		validateService = ServiceProvider.getInstance().getValidateService();
-		validateService.addServiceListener(new IServiceListener() {
-			@Override
-			public void update() {
-				if (areViolationsShown()) {
-					refreshDrawing();
-				}
-			}
-		});
-	}
-
-	@Override
-	public void refreshDrawing() {
-		super.notifyServiceListeners();
-		getAndDrawModulesIn(getCurrentPaths());
-	}
-
-	@Override
-	public void showViolations() {
-		if (validateService.isValidated()) {
-			super.showViolations();
-		}
+		this.initializeServices();
 	}
 
 	@Override
 	public void drawArchitecture(DrawingDetail detail) {
-		super.drawArchitecture(getCurrentDrawingDetail());
+		super.drawArchitecture(this.getCurrentDrawingDetail());
 		super.notifyServiceListeners();
-		AbstractDTO[] modules = analyseService.getRootModules();
-		resetCurrentPaths();
+		AbstractDTO[] modules = this.analyseService.getRootModules();
+		this.resetCurrentPaths();
 		if (DrawingDetail.WITH_VIOLATIONS == detail) {
-			showViolations();
+			this.showViolations();
 		}
-		drawModulesAndLines(modules);
-	}
-
-	@Override
-	protected DependencyDTO[] getDependenciesBetween(BaseFigure figureFrom, BaseFigure figureTo) {
-		AnalysedModuleDTO dtoFrom = (AnalysedModuleDTO) getFigureMap().getModuleDTO(figureFrom);
-		AnalysedModuleDTO dtoTo = (AnalysedModuleDTO) getFigureMap().getModuleDTO(figureTo);
-		if (!figureFrom.equals(figureTo) && null != dtoFrom && null != dtoTo) {
-			return analyseService.getDependencies(dtoFrom.uniqueName, dtoTo.uniqueName);
-		}
-		return new DependencyDTO[] {};
-	}
-
-	@Override
-	protected ViolationDTO[] getViolationsBetween(BaseFigure figureFrom, BaseFigure figureTo) {
-		AnalysedModuleDTO dtoFrom = (AnalysedModuleDTO) getFigureMap().getModuleDTO(figureFrom);
-		AnalysedModuleDTO dtoTo = (AnalysedModuleDTO) getFigureMap().getModuleDTO(figureTo);
-		return validateService.getViolationsByPhysicalPath(dtoFrom.uniqueName, dtoTo.uniqueName);
-	}
-
-	private void resetContextFigures() {
-		analysedContextFigures = new ArrayList<BaseFigure>();
-	}
-
-	@Override
-	public void moduleZoom(BaseFigure[] figures) {
-		super.notifyServiceListeners();
-		resetContextFigures();
-		ArrayList<String> parentNames = new ArrayList<String>();
-		for (BaseFigure figure : figures) {
-			if (figure.isModule()) {
-				try {
-					AnalysedModuleDTO parentDTO = (AnalysedModuleDTO) getFigureMap().getModuleDTO(figure);
-					parentNames.add(parentDTO.uniqueName);
-				} catch (Exception e) {
-					e.printStackTrace();
-					logger.warn("Could not zoom on this object: " + figure.getName() + ". Expected a different DTO type.");
-				}
-			} else if (!figure.isLine()) {
-				analysedContextFigures.add(figure);
-				logger.warn("Could not zoom on this object: " + figure.getName() + ". Not a module to zoom on. Figure is accepted as context for multizoom.");
-			} else {
-				logger.warn("Could not zoom on this object: " + figure.getName() + ". Not a module to zoom on.");
-			}
-		}
-
-		if (parentNames.size() > 0) {
-			saveSingleLevelFigurePositions();
-			getAndDrawModulesIn(parentNames.toArray(new String[] {}));
-		}
-	}
-
-	@Override
-	public void moduleZoomOut() {
-		super.notifyServiceListeners();
-		if (getCurrentPaths().length > 0) {
-			saveSingleLevelFigurePositions();
-			resetContextFigures();
-			String firstCurrentPaths = getCurrentPaths()[0];
-			AnalysedModuleDTO parentDTO = analyseService.getParentModuleForModule(firstCurrentPaths);
-			if (null != parentDTO) {
-				getAndDrawModulesIn(parentDTO.uniqueName);
-			} else {
-				logger.warn("Tried to zoom out from \"" + getCurrentPaths() + "\", but it has no parent (could be root if it's an empty string).");
-				logger.debug("Reverting to the root of the application.");
-				drawArchitecture(getCurrentDrawingDetail());
-			}
-		} else {
-			logger.warn("Tried to zoom out from \"" + getCurrentPaths() + "\", but it has no parent (could be root if it's an empty string).");
-			logger.debug("Reverting to the root of the application.");
-			drawArchitecture(getCurrentDrawingDetail());
-		}
+		this.drawModulesAndLines(modules);
 	}
 
 	private void getAndDrawModulesIn(String parentName) {
-		AnalysedModuleDTO[] children = analyseService.getChildModulesInModule(parentName);
+		AnalysedModuleDTO[] children = this.analyseService
+				.getChildModulesInModule(parentName);
 		if (parentName.equals("")) {
-			drawArchitecture(getCurrentDrawingDetail());
+			this.drawArchitecture(this.getCurrentDrawingDetail());
 		} else if (children.length > 0) {
-			setCurrentPaths(new String[] { parentName });
-			drawModulesAndLines(children);
+			this.setCurrentPaths(new String[] { parentName });
+			this.drawModulesAndLines(children);
 		} else {
-			logger.warn("Tried to draw modules for \"" + parentName + "\", but it has no children.");
+			this.logger.warn("Tried to draw modules for \"" + parentName
+					+ "\", but it has no children.");
 		}
 	}
 
 	private void getAndDrawModulesIn(String[] parentNames) {
 		if (parentNames.length == 0) {
-			drawArchitecture(getCurrentDrawingDetail());
+			this.drawArchitecture(this.getCurrentDrawingDetail());
 		} else {
 			HashMap<String, ArrayList<AbstractDTO>> allChildren = new HashMap<String, ArrayList<AbstractDTO>>();
 			for (String parentName : parentNames) {
-				AbstractDTO[] children = analyseService.getChildModulesInModule(parentName);
+				AbstractDTO[] children = this.analyseService
+						.getChildModulesInModule(parentName);
 				if (parentName.equals("")) {
-					drawArchitecture(getCurrentDrawingDetail());
+					this.drawArchitecture(this.getCurrentDrawingDetail());
 					continue;
 				} else if (children.length > 0) {
 					ArrayList<AbstractDTO> knownChildren = new ArrayList<AbstractDTO>();
@@ -173,18 +73,21 @@ public class AnalysedController extends DrawingController {
 					}
 					allChildren.put(parentName, knownChildren);
 				} else {
-					logger.warn("Tried to draw modules for \"" + parentName + "\", but it has no children.");
+					this.logger.warn("Tried to draw modules for \""
+							+ parentName + "\", but it has no children.");
 				}
 			}
-			if (analysedContextFigures.size() > 0) {
+			if (this.analysedContextFigures.size() > 0) {
 				ArrayList<AbstractDTO> tmp = new ArrayList<AbstractDTO>();
-				for (BaseFigure figure : analysedContextFigures) {
+				for (BaseFigure figure : this.analysedContextFigures) {
 					if (!figure.isLine() && !figure.isParent()) {
-						AbstractDTO dto = getFigureMap().getModuleDTO(figure);
+						AbstractDTO dto = this.getFigureMap().getModuleDTO(
+								figure);
 						if (null != dto) {
 							tmp.add(dto);
 						} else {
-							logger.debug(figure.getName() + " -> " + figure);
+							this.logger.debug(figure.getName() + " -> "
+									+ figure);
 						}
 					}
 				}
@@ -192,27 +95,157 @@ public class AnalysedController extends DrawingController {
 					allChildren.put("", tmp);
 				}
 			}
-			setCurrentPaths(parentNames);
+			this.setCurrentPaths(parentNames);
 
 			Set<String> parentNamesKeySet = allChildren.keySet();
 			if (parentNamesKeySet.size() == 1) {
 				String onlyParentModule = parentNamesKeySet.iterator().next();
-				ArrayList<AbstractDTO> onlyParentChildren = allChildren.get(onlyParentModule);
-				drawModulesAndLines(onlyParentChildren.toArray(new AbstractDTO[] {}));
+				ArrayList<AbstractDTO> onlyParentChildren = allChildren
+						.get(onlyParentModule);
+				this.drawModulesAndLines(onlyParentChildren
+						.toArray(new AbstractDTO[] {}));
 			} else {
-				drawModulesAndLines(allChildren);
+				this.drawModulesAndLines(allChildren);
 			}
 		}
 	}
 
 	@Override
+	protected DependencyDTO[] getDependenciesBetween(BaseFigure figureFrom,
+			BaseFigure figureTo) {
+		AnalysedModuleDTO dtoFrom = (AnalysedModuleDTO) this.getFigureMap()
+				.getModuleDTO(figureFrom);
+		AnalysedModuleDTO dtoTo = (AnalysedModuleDTO) this.getFigureMap()
+				.getModuleDTO(figureTo);
+		if (!figureFrom.equals(figureTo) && null != dtoFrom && null != dtoTo) {
+			return this.analyseService.getDependencies(dtoFrom.uniqueName,
+					dtoTo.uniqueName);
+		}
+		return new DependencyDTO[] {};
+	}
+
+	@Override
+	protected ViolationDTO[] getViolationsBetween(BaseFigure figureFrom,
+			BaseFigure figureTo) {
+		AnalysedModuleDTO dtoFrom = (AnalysedModuleDTO) this.getFigureMap()
+				.getModuleDTO(figureFrom);
+		AnalysedModuleDTO dtoTo = (AnalysedModuleDTO) this.getFigureMap()
+				.getModuleDTO(figureTo);
+		return this.validateService.getViolationsByPhysicalPath(
+				dtoFrom.uniqueName, dtoTo.uniqueName);
+	}
+
+	private void initializeServices() {
+		this.analyseService = ServiceProvider.getInstance().getAnalyseService();
+		this.analyseService.addServiceListener(new IServiceListener() {
+			@Override
+			public void update() {
+				AnalysedController.this.refreshDrawing();
+			}
+		});
+		this.validateService = ServiceProvider.getInstance()
+				.getValidateService();
+		this.validateService.addServiceListener(new IServiceListener() {
+			@Override
+			public void update() {
+				if (AnalysedController.this.areViolationsShown()) {
+					AnalysedController.this.refreshDrawing();
+				}
+			}
+		});
+	}
+
+	@Override
 	public void moduleOpen(String[] paths) {
 		super.notifyServiceListeners();
-		resetContextFigures();
+		this.resetContextFigures();
 		if (paths.length == 0) {
-			drawArchitecture(getCurrentDrawingDetail());
+			this.drawArchitecture(this.getCurrentDrawingDetail());
 		} else {
-			getAndDrawModulesIn(paths);
+			this.getAndDrawModulesIn(paths);
+		}
+	}
+
+	@Override
+	public void moduleZoom(BaseFigure[] figures) {
+		super.notifyServiceListeners();
+		this.resetContextFigures();
+		ArrayList<String> parentNames = new ArrayList<String>();
+		for (BaseFigure figure : figures) {
+			if (figure.isModule()) {
+				try {
+					AnalysedModuleDTO parentDTO = (AnalysedModuleDTO) this
+							.getFigureMap().getModuleDTO(figure);
+					parentNames.add(parentDTO.uniqueName);
+				} catch (Exception e) {
+					e.printStackTrace();
+					this.logger.warn("Could not zoom on this object: "
+							+ figure.getName()
+							+ ". Expected a different DTO type.");
+				}
+			} else if (!figure.isLine()) {
+				this.analysedContextFigures.add(figure);
+				this.logger
+						.warn("Could not zoom on this object: "
+								+ figure.getName()
+								+ ". Not a module to zoom on. Figure is accepted as context for multizoom.");
+			} else {
+				this.logger.warn("Could not zoom on this object: "
+						+ figure.getName() + ". Not a module to zoom on.");
+			}
+		}
+
+		// TODO: Add everything to analysedContextFigures.
+
+		if (parentNames.size() > 0) {
+			this.saveSingleLevelFigurePositions();
+			this.getAndDrawModulesIn(parentNames.toArray(new String[] {}));
+		}
+	}
+
+	@Override
+	public void moduleZoomOut() {
+		super.notifyServiceListeners();
+		if (this.getCurrentPaths().length > 0) {
+			this.saveSingleLevelFigurePositions();
+			this.resetContextFigures();
+			String firstCurrentPaths = this.getCurrentPaths()[0];
+			AnalysedModuleDTO parentDTO = this.analyseService
+					.getParentModuleForModule(firstCurrentPaths);
+			if (null != parentDTO) {
+				this.getAndDrawModulesIn(parentDTO.uniqueName);
+			} else {
+				this.logger
+						.warn("Tried to zoom out from \""
+								+ this.getCurrentPaths()
+								+ "\", but it has no parent (could be root if it's an empty string).");
+				this.logger.debug("Reverting to the root of the application.");
+				this.drawArchitecture(this.getCurrentDrawingDetail());
+			}
+		} else {
+			this.logger
+					.warn("Tried to zoom out from \""
+							+ this.getCurrentPaths()
+							+ "\", but it has no parent (could be root if it's an empty string).");
+			this.logger.debug("Reverting to the root of the application.");
+			this.drawArchitecture(this.getCurrentDrawingDetail());
+		}
+	}
+
+	@Override
+	public void refreshDrawing() {
+		super.notifyServiceListeners();
+		this.getAndDrawModulesIn(this.getCurrentPaths());
+	}
+
+	private void resetContextFigures() {
+		this.analysedContextFigures = new ArrayList<BaseFigure>();
+	}
+
+	@Override
+	public void showViolations() {
+		if (this.validateService.isValidated()) {
+			super.showViolations();
 		}
 	}
 }
