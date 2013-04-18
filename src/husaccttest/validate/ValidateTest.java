267056a9bba1533a04cb890527e2880ba2b7410e
Timothe Beemster <t.beemster@drecomm.nl>
2013-04-18 14:13:33 +0200

    [CLEANUP] Refactor validate test to comply to Java Standards

--- src/husaccttest/validate/ValidateTest.java
+++ src/husaccttest/validate/ValidateTest.java
@@ -23,155 +23,153 @@ import org.junit.Test;
 
 public class ValidateTest {
 
-	IDefineService define;
-	IValidateService validate;
+    IDefineService define;
+    IValidateService validate;
 
-	@Before
-	public void setup() {
-		ServiceProvider.getInstance().getControlService();
-		this.define = ServiceProvider.getInstance().getDefineService();
-		this.validate = ServiceProvider.getInstance().getValidateService();
-	}
+    @Before
+    public void setup() {
+	ServiceProvider.getInstance().getControlService();
+	this.define = ServiceProvider.getInstance().getDefineService();
+	this.validate = ServiceProvider.getInstance().getValidateService();
+    }
 
-	@Test
-	public void getBrowseViolationsGUI() {
-		Object screen = validate.getBrowseViolationsGUI();
-		assertNotNull(screen);
-		assertTrue(screen instanceof javax.swing.JInternalFrame);
-		assertFalse(((JInternalFrame) screen).isVisible());
-	}
+    @Test
+    public void getBrowseViolationsGUI() {
+	Object screen = validate.getBrowseViolationsGUI();
+	assertNotNull(screen);
+	assertTrue(screen instanceof javax.swing.JInternalFrame);
+	assertFalse(((JInternalFrame) screen).isVisible());
+    }
 
-	@Test
-	public void getConfigurationGUI() {
-		Object screen = validate.getConfigurationGUI();
-		assertNotNull(screen);
-		assertTrue(screen instanceof javax.swing.JInternalFrame);
-		assertFalse(((JInternalFrame) screen).isVisible());
-	}
+    @Test
+    public void getConfigurationGUI() {
+	Object screen = validate.getConfigurationGUI();
+	assertNotNull(screen);
+	assertTrue(screen instanceof javax.swing.JInternalFrame);
+	assertFalse(((JInternalFrame) screen).isVisible());
+    }
 
-	@Test
-	public void getExportExtentions() {
-		assertArrayEquals(new String[] {"pdf", "html", "xml"}, validate.getExportExtentions());
-	}
+    @Test
+    public void getExportExtentions() {
+	assertArrayEquals(new String[]{"pdf", "html", "xml"}, validate.getExportExtentions());
+    }
 
-	@Test
-	public void exportViolations() {
-		// can't test void
-	}
+    @Test
+    public void exportViolations() {
+	// can't test void
+    }
 
-	@Test
-	public void getCategories() {
-		CategoryDTO[] dtos = validate.getCategories();
-		assertArrayEquals(new String[] {"contentsofamodule", "legalityofdependency"}, getCategoryStringArray(dtos));
-	}
+    @Test
+    public void getCategories() {
+	CategoryDTO[] dtos = validate.getCategories();
+	assertArrayEquals(new String[]{"contentsofamodule", "legalityofdependency"}, getCategoryStringArray(dtos));
+    }
 
-	@Test
-	public void getRuleTypes() {
-		CategoryDTO[] dtos = validate.getCategories();
-		final String[] currentRuletypes = new String[] {"InterfaceConvention", "NamingConvention", "FacadeConvention", "SubClassConvention", "VisibilityConvention", "IsNotAllowedToUse", "IsOnlyAllowedToUse", "IsNotAllowedToMakeSkipCall", "IsOnlyModuleAllowedToUse", "MustUse", "IsNotAllowedToMakeBackCall"};
-		assertArrayEquals(currentRuletypes, getRuleTypesStringArray(dtos));
+    @Test
+    public void getRuleTypes() {
+	CategoryDTO[] dtos = validate.getCategories();
+	final String[] currentRuletypes = new String[]{"InterfaceConvention", "NamingConvention", "FacadeConvention", "SubClassConvention", "VisibilityConvention", "IsNotAllowedToUse", "IsOnlyAllowedToUse", "IsNotAllowedToMakeSkipCall", "IsOnlyModuleAllowedToUse", "MustUse", "IsNotAllowedToMakeBackCall"};
+	assertArrayEquals(currentRuletypes, getRuleTypesStringArray(dtos));
 
-	}
+    }
 
-	@Test
-	public void getViolationTypesJavaLanguage() {
+    @Test
+    public void getViolationTypesJavaLanguage() {
 //		define.createApplication("", new String[] {}, "Java", "");
-		CategoryDTO[] dtos = validate.getCategories();
-		assertEquals(12, getViolationTypesStringArray(dtos, "IsNotAllowedToUse").length);
-		assertEquals(12, getViolationTypesStringArray(dtos, "IsAllowedToUse").length);
-		assertEquals(4, getViolationTypesStringArray(dtos, "VisibilityConvention").length);
-	}
+	CategoryDTO[] dtos = validate.getCategories();
+	assertEquals(12, getViolationTypesStringArray(dtos, "IsNotAllowedToUse").length);
+	assertEquals(12, getViolationTypesStringArray(dtos, "IsAllowedToUse").length);
+	assertEquals(4, getViolationTypesStringArray(dtos, "VisibilityConvention").length);
+    }
 
-	@Test
-	public void getViolationTypesCSharpLanguage() {
+    @Test
+    public void getViolationTypesCSharpLanguage() {
 //		define.createApplication("", new String[] {}, "C#", "");
-		CategoryDTO[] dtos = validate.getCategories();
-		assertEquals(12, getViolationTypesStringArray(dtos, "IsNotAllowedToUse").length);
-		assertEquals(12, getViolationTypesStringArray(dtos, "IsAllowedToUse").length);
-		assertEquals(4, getViolationTypesStringArray(dtos, "VisibilityConvention").length);
-	}
+	CategoryDTO[] dtos = validate.getCategories();
+	assertEquals(12, getViolationTypesStringArray(dtos, "IsNotAllowedToUse").length);
+	assertEquals(12, getViolationTypesStringArray(dtos, "IsAllowedToUse").length);
+	assertEquals(4, getViolationTypesStringArray(dtos, "VisibilityConvention").length);
+    }
 
-	@Test
-	public void getViolationTypesNoLanguage() {
+    @Test
+    public void getViolationTypesNoLanguage() {
 //		define.createApplication("", new String[] {}, "", "");
-		CategoryDTO[] dtos = validate.getCategories();
-		assertEquals(0, getViolationTypesStringArray(dtos, "IsNotAllowedToUse").length);
-		assertEquals(0, getViolationTypesStringArray(dtos, "IsAllowedToUse").length);
-	}
+	CategoryDTO[] dtos = validate.getCategories();
+	assertEquals(0, getViolationTypesStringArray(dtos, "IsNotAllowedToUse").length);
+	assertEquals(0, getViolationTypesStringArray(dtos, "IsAllowedToUse").length);
+    }
 
-	private String[] getCategoryStringArray(CategoryDTO[] dtos) {
-		List<String> categoryList = new ArrayList<String>();
-		for (CategoryDTO dto : dtos) {
-			categoryList.add(dto.getKey());
-		}
-		return categoryList.toArray(new String[] {});
+    private String[] getCategoryStringArray(CategoryDTO[] dtos) {
+	List<String> categoryList = new ArrayList<String>();
+	for (CategoryDTO dto : dtos) {
+	    categoryList.add(dto.getKey());
 	}
+	return categoryList.toArray(new String[]{});
+    }
 
-	private String[] getRuleTypesStringArray(CategoryDTO[] dtos) {
-		List<String> ruletypeList = new ArrayList<String>();
-		for (CategoryDTO cDTO : dtos) {
-			for (RuleTypeDTO rDTO : cDTO.getRuleTypes()) {
-				ruletypeList.add(rDTO.getKey());
+    private String[] getRuleTypesStringArray(CategoryDTO[] dtos) {
+	List<String> ruletypeList = new ArrayList<String>();
+	for (CategoryDTO cDTO : dtos) {
+	    for (RuleTypeDTO rDTO : cDTO.getRuleTypes()) {
+		ruletypeList.add(rDTO.getKey());
+	    }
+	}
+	return ruletypeList.toArray(new String[]{});
+    }
+
+    private String[] getViolationTypesStringArray(CategoryDTO[] dtos, String ruleTypeKey) {
+	List<String> violationtypeList = new ArrayList<String>();
+	for (CategoryDTO cDTO : dtos) {
+	    for (RuleTypeDTO rDTO : cDTO.getRuleTypes()) {
+		if (rDTO.getKey().equals(ruleTypeKey)) {
+		    return getViolationTypesStringArray(rDTO);
+		} else {
+		    for (RuleTypeDTO exceptionDTO : rDTO.getExceptionRuleTypes()) {
+			if (exceptionDTO.getKey().equals(ruleTypeKey)) {
+			    return getViolationTypesStringArray(exceptionDTO);
 			}
+		    }
 		}
-		return ruletypeList.toArray(new String[] {});
+	    }
+	}
+	return violationtypeList.toArray(new String[]{});
+    }
+
+    private String[] getViolationTypesStringArray(RuleTypeDTO rule) {
+	List<String> violationTypeList = new ArrayList<String>();
+	for (ViolationTypeDTO vDTO : rule.getViolationTypes()) {
+	    violationTypeList.add(vDTO.getKey());
+	}
+	return violationTypeList.toArray(new String[]{});
+    }
+
+    @Test
+    public void isValidatedBeforeValidation() {
+	assertFalse(validate.isValidated());
+    }
+
+    @Test
+    public void getViolationsByLogicalPath() {
+	// Can't test this now, need to find a way with define (and analyze) to test this method
+    }
+
+    @Test
+    public void getViolationsByPhysicalPath() {
+	// Can't test this now, need to find a way with define (and analyze) to test this method
+    }
+
+    @Test
+    public void isValidatedAfterValidation() {
+	boolean exceptionOccured = false;
+	try {
+	    validate.checkConformance();
+	} catch (ProgrammingLanguageNotFoundException e) {
+	    exceptionOccured = true;
+	    assertFalse(validate.isValidated());
 	}
 
-	private String[] getViolationTypesStringArray(CategoryDTO[] dtos, String ruleTypeKey) {
-		List<String> violationtypeList = new ArrayList<String>();
-		for (CategoryDTO cDTO : dtos) {
-			for (RuleTypeDTO rDTO : cDTO.getRuleTypes()) {
-				if (rDTO.getKey().equals(ruleTypeKey)) {
-					return getViolationTypesStringArray(rDTO);
-				}
-				else {
-					for (RuleTypeDTO exceptionDTO : rDTO.getExceptionRuleTypes()) {
-						if (exceptionDTO.getKey().equals(ruleTypeKey)) {
-							return getViolationTypesStringArray(exceptionDTO);
-						}
-					}
-				}
-			}
-		}
-		return violationtypeList.toArray(new String[] {});
+	if (!exceptionOccured) {
+	    assertTrue(validate.isValidated());
 	}
-
-	private String[] getViolationTypesStringArray(RuleTypeDTO rule) {
-		List<String> violationTypeList = new ArrayList<String>();
-		for (ViolationTypeDTO vDTO : rule.getViolationTypes()) {
-			violationTypeList.add(vDTO.getKey());
-		}
-		return violationTypeList.toArray(new String[] {});
-	}
-
-	@Test
-	public void isValidatedBeforeValidation() {
-		assertFalse(validate.isValidated());
-	}
-
-	@Test
-	public void getViolationsByLogicalPath() {
-		// Can't test this now, need to find a way with define (and analyze) to test this method
-	}
-
-	@Test
-	public void getViolationsByPhysicalPath() {
-		// Can't test this now, need to find a way with define (and analyze) to test this method
-	}
-
-	@Test
-	public void isValidatedAfterValidation() {
-		boolean exceptionOccured = false;
-		try {
-			validate.checkConformance();
-		}
-		catch (ProgrammingLanguageNotFoundException e) {
-			exceptionOccured = true;
-			assertFalse(validate.isValidated());
-		}
-
-		if (!exceptionOccured) {
-			assertTrue(validate.isValidated());
-		}
-	}
+    }
 }
