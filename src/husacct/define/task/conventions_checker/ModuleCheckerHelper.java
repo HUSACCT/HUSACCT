346afc04408ddf0df475955df924f106ef59829b
Xeroti <bayram@live.nl>
2013-05-08 14:35:01 +0200

    Removed test string in rule conventions checker

--- src/husacct/define/task/conventions_checker/ModuleCheckerHelper.java
+++ src/husacct/define/task/conventions_checker/ModuleCheckerHelper.java
@@ -21,7 +21,6 @@ public class ModuleCheckerHelper {
 	public boolean checkRuleTypeAlreadySet(String ruleTypeKey, Module moduleFrom) {
 		for(AppliedRule appliedRule : this.getFromModuleAppliedRules(moduleFrom)) {
 			if(appliedRule.getRuleType().equals(ruleTypeKey)) {
-				System.out.println(ruleTypeKey);
 				setErrorMessage("'"+ ServiceProvider.getInstance().getLocaleService().getTranslatedString(ruleTypeKey) + "'");
 				return false;
 			}
