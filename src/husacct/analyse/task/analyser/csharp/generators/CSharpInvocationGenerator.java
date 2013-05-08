9e06671c3411bc587112cdf3cec99ee00209879b
Bosmagit <gmbosma@gmail.com>
2013-05-08 19:51:50 +0200

    Constructor invocation with namespace prefix are now recognized

--- src/husacct/analyse/task/analyser/csharp/generators/CSharpInvocationGenerator.java
+++ src/husacct/analyse/task/analyser/csharp/generators/CSharpInvocationGenerator.java
@@ -2,6 +2,9 @@ package husacct.analyse.task.analyser.csharp.generators;
 
 
 
+import java.util.ArrayList;
+
+import husacct.analyse.infrastructure.antlr.TreePrinter;
 import husacct.analyse.infrastructure.antlr.csharp.CSharpParser;
 import static husacct.analyse.task.analyser.csharp.generators.CSharpGeneratorToolkit.*;
 
@@ -20,6 +23,7 @@ public class CSharpInvocationGenerator extends CSharpGenerator {
 	private boolean constructorInMethodInvocationFound = false;
 	private boolean foundAllMethodInvocInfo = false;
 	private boolean allIdents = true;
+	private ArrayList<String> toNames = new ArrayList<String>();
 	
 	
 	public CSharpInvocationGenerator(String uniqueClassName) {
@@ -27,9 +31,10 @@ public class CSharpInvocationGenerator extends CSharpGenerator {
 	}
 
 	public void generateConstructorInvocToDomain(CommonTree treeNode, String belongsToMethod) {
+		
 		invocationName = "Constructor";
 		this.belongsToMethod = belongsToMethod;
-		createConstructorInvocationDetails(treeNode);
+		findConstructorInvocation(treeNode);
 		createConstructorInvocationDomainObject();
 	}
 	
@@ -46,10 +51,10 @@ public class CSharpInvocationGenerator extends CSharpGenerator {
 		// TODO Auto-generated method stub	
 	}
 
-	private void createConstructorInvocationDetails(CommonTree tree) {
+	private void findConstructorInvocation(CommonTree tree) {
 		boolean constructorFound = hasConstructorCall(tree);
 		if (constructorFound){
-			createConstructorInvocationDetailsWhenFoundClassConstructorCall((CommonTree) tree);
+			createConstructorInvocationDetails((CommonTree) tree);
 		}
 		else {
 			int childcount = tree.getChildCount();
@@ -57,28 +62,52 @@ public class CSharpInvocationGenerator extends CSharpGenerator {
 				CommonTree child =  (CommonTree) tree.getChild(i);
 
 				if (hasConstructorCall(child)) { 
-					createConstructorInvocationDetailsWhenFoundClassConstructorCall(child);
+					createConstructorInvocationDetails(child);
 				}
-				createConstructorInvocationDetails(child);
+				findConstructorInvocation(child);
 			}
 		}
 	}
 		
 	
-	private CommonTree getDetailsForConstructorInvocationTree(CommonTree tree) {
-		return walkTree(tree, CSharpParser.TYPE, CSharpParser.NAMESPACE_OR_TYPE_NAME, CSharpParser.IDENTIFIER);
-	}
+	private void createConstructorInvocationDetails(CommonTree tree) {	
+		this.to = setToName(tree);
+		this.lineNumber = tree.getChild(0).getLine();
 
-	private void createConstructorInvocationDetailsWhenFoundClassConstructorCall(CommonTree tree) {	
-		CommonTree constructorInvocationDetailsTree = getDetailsForConstructorInvocationTree(tree);
-		if (constructorInvocationDetailsTree != null) {
-			this.to = constructorInvocationDetailsTree.getText();
-			this.lineNumber = constructorInvocationDetailsTree.getLine();
-		}
 	}
 	
+	private String setToName(CommonTree tree) {
+		String toName = "";
+		walkTreeToGetNames(tree);	
+		
+		for (String name : toNames) {
+			toName += name + ".";
+		}
+		toName = toName.substring(0, toName.length()-1); // remove last dot
+		
+		return toName;
+	}
+	
+	private void walkTreeToGetNames(CommonTree tree) {
+		for (int i = 0; i < tree.getChildCount(); i++) {
+			CommonTree child = (CommonTree)tree.getChild(i);
+			switch (child.getType()) {
+			case CSharpParser.NAMESPACE_OR_TYPE_NAME:
+			case CSharpParser.NAMESPACE_OR_TYPE_PART:	
+				String name = child.getFirstChildWithType(CSharpParser.IDENTIFIER).getText();
+				this.toNames.add(name);
+				break;
+			}
+			walkTreeToGetNames(child);
+		}	
+	}
+
 	private void createConstructorInvocationDomainObject() {
 		modelService.createConstructorInvocation(from, to, lineNumber, invocationName, belongsToMethod, nameOfInstance);
+	}
+	
+	private void createMethodInvocationDomainObject() {
+		modelService.createMethodInvocation(from, to, lineNumber, invocationName, belongsToMethod, nameOfInstance);
 	}
 	
 	private boolean hasConstructorCall(CommonTree tree) {
