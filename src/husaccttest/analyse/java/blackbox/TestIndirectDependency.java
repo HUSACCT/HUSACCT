14564144c8c75a119cb9a65ca4d0bd2209ba2370
Twan <twanverboom@outlook.com>
2013-05-14 00:52:53 +0200

    Just a step to prevent the multithreading to break our test

--- src/husaccttest/analyse/java/blackbox/TestIndirectDependency.java
+++ src/husaccttest/analyse/java/blackbox/TestIndirectDependency.java
@@ -1,7 +1,9 @@
 package husaccttest.analyse.java.blackbox;
 
+import husacct.ServiceProvider;
 import husacct.common.dto.DependencyDTO;
 import husacct.common.dto.ExternalSystemDTO;
+import husacct.control.task.States;
 import husaccttest.analyse.TestCaseExtended;
 
 import org.junit.Test;
@@ -10,7 +12,13 @@ public class TestIndirectDependency extends TestCaseExtended  {
 	
 	@Test
 	public void testCheck(){
+		// important so it won't break during analysing.
+		ServiceProvider.getInstance().getControlService().getState().add(States.ANALYSING);
+		
 		DependencyDTO[] dependencies = service.getAllDependencies();
+		
+		// reset the state of the analyser.
+		ServiceProvider.getInstance().getControlService().getState().remove(States.ANALYSING);
 		
 		printDependencies(dependencies);
 		
