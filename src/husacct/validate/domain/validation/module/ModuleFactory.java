e2093ae4dfb5a8d2f00528f1cb8578f793a415f0
McRonald <ronaldvanbroeckhuijsen@hotmail.com>
2013-05-17 12:38:16 +0200

    updated module allowed rule type settings

--- src/husacct/validate/domain/validation/module/ModuleFactory.java
+++ src/husacct/validate/domain/validation/module/ModuleFactory.java
@@ -6,19 +6,19 @@ import husacct.validate.domain.exception.ModuleNotFoundException;
 import husacct.validate.domain.validation.ruletype.RuleType;
 
 public class ModuleFactory {
-	public AbstractModule createModule(String type, List<RuleType> rules) throws ModuleNotFoundException {
-		
-		switch(type.toLowerCase()) {
-			case "component": 
-				return new Component(rules);
-			case "externallibrary": 
-				return new ExternalLibrary(rules);
-			case "layer": 	
-				return new Layer(rules);
-			case "subsystem": 
-				return new SubSystem(rules);
-			default:
-				throw new ModuleNotFoundException(type);
-		}
+    public AbstractModule createModule(String type, List<RuleType> rules) throws ModuleNotFoundException {
+
+	switch (type.toLowerCase()) {
+	case "component":
+	    return new Component(rules);
+	case "externallibrary":
+	    return new ExternalLibrary(rules);
+	case "layer":
+	    return new Layer(rules);
+	case "subsystem":
+	    return new SubSystem(rules);
+	default:
+	    throw new ModuleNotFoundException(type);
 	}
+    }
 }
