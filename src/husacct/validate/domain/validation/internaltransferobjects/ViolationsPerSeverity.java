4218cdb111841d85263100a792a4ecd1647cbebf
Timothe Beemster <t.beemster@drecomm.nl>
2013-05-23 15:51:31 +0200

    [CLEANUP] Refactor remove underlines in package internal_transfer_objects

--- src/husacct/validate/domain/validation/internaltransferobjects/ViolationsPerSeverity.java
+++ src/husacct/validate/domain/validation/internaltransferobjects/ViolationsPerSeverity.java
@@ -1,4 +1,4 @@
-package husacct.validate.domain.validation.internal_transfer_objects;
+package husacct.validate.domain.validation.internaltransferobjects;
 
 import husacct.validate.domain.validation.Severity;
 
