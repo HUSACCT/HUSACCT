c25412a4e18d6cd4a7d11a630cc77db09237ad2f
Ali Korkmaz <aleeyk@hotmail.com>
2013-05-31 15:03:55 +0200

    Returning new object for selecting programming languages

--- src/husacct/analyse/task/analyser/AnalyserBuilder.java
+++ src/husacct/analyse/task/analyser/AnalyserBuilder.java
@@ -6,14 +6,12 @@ import husacct.analyse.task.analyser.java.JavaAnalyser;
 class AnalyserBuilder {
 
     public AbstractAnalyser getAnalyser(String language) {
-        AbstractAnalyser applicationAnalyser;
         if (language.equals(new JavaAnalyser().getProgrammingLanguage())) {
-            applicationAnalyser = new JavaAnalyser();
+            return new JavaAnalyser();
         } else if (language.equals(new CSharpAnalyser().getProgrammingLanguage())) {
-            applicationAnalyser = new CSharpAnalyser();
+            return new CSharpAnalyser();
         } else {
-            applicationAnalyser = null;
+            return null;
         }
-        return applicationAnalyser;
     }
 }
