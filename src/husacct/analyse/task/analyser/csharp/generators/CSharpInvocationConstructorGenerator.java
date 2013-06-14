5830e07900e064ddc160a6b1f21e446d5c935d38
Bosmagit <gmbosma@gmail.com>
2013-06-14 12:37:07 +0200

    Fixes #40, refactored code

--- src/husacct/analyse/task/analyser/csharp/generators/CSharpInvocationConstructorGenerator.java
+++ src/husacct/analyse/task/analyser/csharp/generators/CSharpInvocationConstructorGenerator.java
@@ -1,13 +1,10 @@
 package husacct.analyse.task.analyser.csharp.generators;
 
-import java.util.ArrayList;
 import husacct.analyse.infrastructure.antlr.csharp.CSharpParser;
 import static husacct.analyse.task.analyser.csharp.generators.CSharpGeneratorToolkit.*;
-
 import org.antlr.runtime.tree.CommonTree;
 
 public class CSharpInvocationConstructorGenerator extends AbstractCSharpInvocationGenerator{
-	private ArrayList<String> toNames = new ArrayList<String>();
 
 	public CSharpInvocationConstructorGenerator(String packageAndClassName) {
 		super(packageAndClassName);
@@ -37,38 +34,8 @@ public class CSharpInvocationConstructorGenerator extends AbstractCSharpInvocati
 	}
 	
 	private void createConstructorInvocationDetails(CommonTree constructorTree) {
-		this.to = setToName(constructorTree);
+		this.to = getTypeNameAndParts(constructorTree);
 		this.lineNumber = constructorTree.getChild(0).getLine();
-	}
-	
-	private String setToName(CommonTree treeNode) {
-		String toName = "";
-		walkTreeToGetNames(treeNode);	
-		
-		for (String name : toNames) {
-			toName += name + ".";
-		}
-		toNames.clear();
-		toName = removeLastDot(toName);
-		return toName;
-	}
-	
-	private void walkTreeToGetNames(CommonTree treeNode) {
-		for (int i = 0; i < treeNode.getChildCount(); i++) {
-			CommonTree child = (CommonTree)treeNode.getChild(i);
-			switch (child.getType()) {
-			case CSharpParser.NAMESPACE_OR_TYPE_NAME:
-			case CSharpParser.NAMESPACE_OR_TYPE_PART:	
-				String name = child.getFirstChildWithType(CSharpParser.IDENTIFIER).getText();
-				this.toNames.add(name);
-				break;
-			}
-			walkTreeToGetNames(child);
-		}
-	}
-	
-	private String removeLastDot(String toName) {
-		return toName.endsWith(".") ? toName.substring(0, toName.length() -1) : toName;
 	}
 
 	private void checkForArguments(CommonTree methodTree) {
