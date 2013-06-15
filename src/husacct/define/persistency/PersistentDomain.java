95e186b27e53f51b1577aa71cd71208a07da2229
Rob Uithol <RobUithol@gmail.com>
2013-06-15 12:49:24 +0200

    Fixed saving bug

--- src/husacct/define/persistency/PersistentDomain.java
+++ src/husacct/define/persistency/PersistentDomain.java
@@ -2,13 +2,20 @@ package husacct.define.persistency;
 
 
 
+import husacct.ServiceProvider;
+import husacct.common.dto.ProjectDTO;
 import husacct.common.savechain.ISaveable;
 import husacct.define.domain.Application;
+import husacct.define.domain.Project;
 import husacct.define.domain.SoftwareArchitecture;
+import husacct.define.domain.appliedrule.AppliedRuleStrategy;
+import husacct.define.domain.module.ModuleStrategy;
 import husacct.define.domain.services.AppliedRuleDomainService;
 import husacct.define.domain.services.AppliedRuleExceptionDomainService;
 import husacct.define.domain.services.ModuleDomainService;
 import husacct.define.domain.services.SoftwareArchitectureDomainService;
+
+import java.util.ArrayList;
 
 import org.jdom2.Element;
 
@@ -64,6 +71,46 @@ public class PersistentDomain implements ISaveable {
 
 		XMLParser = new XMLDomain(workspaceData);
 		workspaceApplication = XMLParser.createApplication();
+		SoftwareArchitecture workspaceArchitecture = workspaceApplication.getArchitecture(); 
+
+		switch (parseData) {
+		default:
+			ArrayList<ProjectDTO> projects = new ArrayList<ProjectDTO>();
+			for (Project project : workspaceApplication.getProjects()) {
+				projects.add(new ProjectDTO(project.getName(), project
+						.getPaths(), project.getProgrammingLanguage(), project
+						.getVersion(), project.getDescription(), null));
+			}
+			ServiceProvider.getInstance().getDefineService().createApplication(workspaceApplication.getName(),
+			projects, 
+			workspaceApplication.getVersion());
+			domainService.createNewArchitectureDefinition(workspaceArchitecture
+					.getName());
+			for (ModuleStrategy m : workspaceArchitecture.getModules()) {
+				long rootModule = moduleService.addModuleToRoot(m);
+				if (m.getSubModules().size() > 0) {
+					for (ModuleStrategy subModule : m.getSubModules()) {
+						moduleService.addModuleToParent(rootModule, subModule);
+					}
+				}
+			}
+			for (AppliedRuleStrategy ApplRule : workspaceArchitecture.getAppliedRules()) {
+				long addedRule = appliedRuleService.addAppliedRule(ApplRule
+						.getRuleType(), ApplRule.getDescription(), ApplRule
+						.getDependencies(), ApplRule.getRegex(), ApplRule
+						.getModuleFrom().getId(), ApplRule.getModuleTo()
+						.getId(), ApplRule.isEnabled());
+				if (ApplRule.getExceptions().size() > 0) {
+					for (AppliedRuleStrategy Ap : ApplRule.getExceptions()) {
+						exceptionService.addExceptionToAppliedRule(addedRule,
+								Ap.getRuleType(), Ap.getDescription(), Ap
+								.getModuleFrom().getId(), Ap
+								.getModuleTo().getId(), Ap
+								.getDependencies());
+					}
+				}
+			}
+		} 
 	}
 
 	/**
