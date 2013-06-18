64f9573b0eee6ead63efe82dde8f8c991822cf87
thijsghu <thijs.schalk@gmail.com>
2013-06-18 15:20:18 +0200

    Moved testprojects commit

--- testprojects/java/accuracy/technology/direct/dao/CheckInDAO.java
+++ testprojects/java/accuracy/technology/direct/dao/CheckInDAO.java
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 package technology.direct.dao;
 
 public class CheckInDAO {
 	public static String currentLocation = "Nijenoord 1, Utrecht";
+=======
+package technology.direct.dao;
+
+public class CheckInDAO {
+	public static String currentLocation = "Nijenoord 1, Utrecht";
+>>>>>>> 7b87626b99107db400440335a38d325b9ac19cd3
 }
--- testprojects/java/accuracy/technology/direct/dao/ProfileDAO.java
+++ testprojects/java/accuracy/technology/direct/dao/ProfileDAO.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package technology.direct.dao;
 
 public class ProfileDAO {
@@ -8,3 +9,15 @@ public class ProfileDAO {
 	}
 
 }
+=======
+package technology.direct.dao;
+
+public class ProfileDAO {
+	public String name = "profile";
+	
+	public String getCampaignType() {
+		return "commercial";
+	}
+
+}
+>>>>>>> 7b87626b99107db400440335a38d325b9ac19cd3
