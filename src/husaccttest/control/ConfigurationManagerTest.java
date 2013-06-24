2a59fd3f5588452f66790d3b56172ee7cecdc73b
Donovan de Kuiper <c391617@rmqkr.net>
2013-06-24 14:01:05 +0200

    Added new test cases

--- src/husaccttest/control/ConfigurationManagerTest.java
+++ src/husaccttest/control/ConfigurationManagerTest.java
@@ -1,13 +1,33 @@
 package husaccttest.control;
 
 import static org.junit.Assert.assertEquals;
+import husacct.control.task.configuration.ConfigurationManager;
+import husacct.control.task.configuration.IConfigListener;
 
 import org.junit.Test;
 
 public class ConfigurationManagerTest {
 
 	@Test
-	public void awesomeTest() {
-		assertEquals(true, true);
+	public void testConfiguration() {
+		ConfigurationManager.setProperty("Test", "Test");
+		ConfigurationManager.storeProperties();
+		assertEquals(ConfigurationManager.getProperty("Test"), "Test");
+	}
+	
+	public void testSingleSetConfiguration() {
+		ConfigurationManager.setPropertyIfEmpty("Test", "Test");
+		ConfigurationManager.setPropertyIfEmpty("Test", "Test2");
+		assertEquals(ConfigurationManager.getProperty("Test"), "Test");
+	}
+	
+	public void testConfigurationListener() {
+		ConfigurationManager.addListener(new IConfigListener() {
+			@Override
+			public void onConfigUpdate() {
+				assertEquals(true, true);
+			}
+		});
+		ConfigurationManager.notifyListeners();
 	}
 }
