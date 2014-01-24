243678e92bfa14f0ef5548b6eb97c05bfc47788a
leohu <leo.pruijt@hu.nl>
2014-01-24 11:29:55 +0100

    Version ugraded to 2.1 in AboutDialog.java. C# documentation source files included.

--- src/husacct/control/presentation/util/AboutDialog.java
+++ src/husacct/control/presentation/util/AboutDialog.java
@@ -41,7 +41,7 @@ public class AboutDialog extends JDialog{
 	private JPanel logoPanel, textPanel, gitForkPanel;
 	private JLabel forkLabel,pictureLabel, husacctLabel, versionLabel, versionNumberLabel;
 	private JButton okButton, creditsButton;
-	private String versionNumber = "2.0";
+	private String versionNumber = "2.1";
 	private MainController mainController;
 	private GridBagConstraints constraint = new GridBagConstraints();
 	private JPanel forkLabelPanel;
