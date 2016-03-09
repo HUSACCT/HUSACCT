9c5a438dfefc4eb9fdc1ab805bb99b3debac48b4
sven <sven@email.com>
2016-03-09 14:32:44 +0100

    analyse: adding class for algorthm 2

---
+++ src/husacct/analyse/task/reconstruct/AlgorithmTwo.java
@@ -0,0 +1,13 @@
+package husacct.analyse.task.reconstruct;
+
+import husacct.common.dto.ModuleDTO;
+
+public class AlgorithmTwo extends AlgorithmGeneral{
+
+	@Override
+	public void define(ModuleDTO selectedModule, int threshold) {
+		System.out.println("second algorithm");
+
+	}
+
+}
