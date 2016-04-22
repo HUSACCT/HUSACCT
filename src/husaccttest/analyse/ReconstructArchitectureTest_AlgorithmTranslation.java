1ada8d112c4dda7b4058c63763a248fccd5d2f1b
sven <sven@svennett.wifi.hu.nl>
2016-04-22 14:46:08 +0200

    analyse: removed warnings

--- src/husaccttest/analyse/ReconstructArchitectureTest_AlgorithmTranslation.java
+++ src/husaccttest/analyse/ReconstructArchitectureTest_AlgorithmTranslation.java
@@ -7,37 +7,16 @@ import java.io.FileNotFoundException;
 import java.lang.reflect.Field;
 import java.util.Scanner;
 
-import org.apache.log4j.Logger;
 import org.junit.Test;
 
-import husacct.analyse.IAnalyseService;
-import husacct.analyse.task.AnalyseTaskControl;
 import husacct.analyse.task.reconstruct.AnalyseReconstructConstants.Algorithm;
-import husacct.common.dto.AnalysisStatisticsDTO;
 import husacct.common.locale.LocaleServiceImpl;
-import husacct.control.ControlServiceImpl;
-import husacct.control.task.MainController;
-import husacct.control.task.WorkspaceController;
-import husacct.define.IDefineService;
 import junit.framework.Assert;
 
 public class ReconstructArchitectureTest_AlgorithmTranslation {
-	private static String workspacePath;
-	private static ControlServiceImpl controlService;
-	private static MainController mainController;
-	private static WorkspaceController workspaceController;
-	private static IDefineService defineService;
-	private final static String workspace = "Workspace_HUSACCT_20_Arch_Without_ANTLR.xml";
-	private static Logger logger;
-	private static IAnalyseService analyseService = null;
 	private static LocaleServiceImpl localeService;
 	
-	private static final String exportFile = "ExportFileAnalysedModel_HUSACCT20_WithoutAntlr.xml";
-	private static String exportFilePath;
-	private static AnalysisStatisticsDTO analyseStatisticsBeforeReconstruction;
-	private static AnalysisStatisticsDTO analyseStatisticsAfterReconstruction;
-	private static AnalyseTaskControl analyseTaskControl;
-	
+	@SuppressWarnings("resource")
 	@Test
 	public void test() throws FileNotFoundException, IllegalArgumentException, IllegalAccessException {
 		localeService = new LocaleServiceImpl();
