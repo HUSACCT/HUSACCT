39621ff5281328323360f6ee7c548b7f64596559
Fabien DUMINY <fduminy@jnode.org>
2018-09-23 18:38:33 +0200

    Ask to save the workspace if it has unsaved changes.

--- src/husacct/control/domain/Workspace.java
+++ src/husacct/control/domain/Workspace.java
@@ -2,31 +2,38 @@ package husacct.control.domain;
 
 import husacct.common.dto.ApplicationDTO;
 
+import java.util.Objects;
+
 public class Workspace {
 	
 	private String name;
 	private ApplicationDTO appData;
+	private boolean dirty;
 
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
+    dirty |= !Objects.equals(this.name, name);
 		this.name = name;
 	}
 	public ApplicationDTO getApplicationData() {
 		return appData;
 	}
 	public void setApplicationData(ApplicationDTO appData) {
+    dirty |= !Objects.deepEquals(this.appData, appData);
 		this.appData = appData;
 	}
-	
-    @Override
+
+	public boolean isDirty() {
+		return dirty;
+	}
+
+	@Override
 	public String toString() {
-        String representation = "";
-        representation += "\nName: " + name;
-        representation += "\n";
-        return representation;
-    }
-	
-	
+    String representation = "";
+    representation += "\nName: " + name;
+    representation += "\n";
+    return representation;
+	}
 }
